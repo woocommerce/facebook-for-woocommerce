d21850cf0b313287fe5db7336d2beba42231e69c
Mengying (Kelly) Du <kellydu@fb.com>
2018-05-02 11:29:33 -0700

    Fix PHP Error from get_the_term
    Summary:
    We added support for brand plugin, however, for the user without brand plugin, get the term will throw WP_error. Then, it throws PHP error when we clean the expected 'string' term.
    
    I checked the call trace and code from Woo official site:
    
    get_the_term_list:https://developer.wordpress.org/reference/functions/get_the_term_list/
    get_the_terms: https://developer.wordpress.org/reference/functions/get_the_term_list/
    get_object_term_cache: https://developer.wordpress.org/reference/functions/get_object_term_cache/
    get_term: https://developer.wordpress.org/reference/functions/get_term/
    
    Reviewed By: sammul
    
    Differential Revision: D7846160
    
    fbshipit-source-id: 04e6080

--- includes/fbproduct.php
+++ includes/fbproduct.php
@@ -438,8 +438,10 @@ class WC_Facebook_Product {
     }
     $categories =
       WC_Facebookcommerce_Utils::get_product_categories($id);
-    $brand = WC_Facebookcommerce_Utils::clean_string(
-      get_the_term_list($this->ID, 'product_brand', '', ', '));
+    $brand = get_the_term_list($this->ID, 'product_brand', '', ', ');
+    $brand = is_wp_error($brand) || !$brand
+      ? WC_Facebookcommerce_Utils::get_store_name()
+      : WC_Facebookcommerce_Utils::clean_string($brand);
 
     $product_data = array(
       'name' => WC_Facebookcommerce_Utils::clean_string(
@@ -449,7 +451,7 @@ class WC_Facebook_Product {
       'additional_image_urls' => array_filter($image_urls),
       'url'=> $product_url,
       'category' => $categories['categories'],
-      'brand' => $brand ?: WC_Facebookcommerce_Utils::get_store_name(),
+      'brand' => $brand,
       'retailer_id' => $retailer_id,
       'price' => $this->get_fb_price(),
       'currency' => get_woocommerce_currency(),
