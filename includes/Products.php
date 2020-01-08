4c05539ec793c2b75ad632d347745d754de4766b
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-01-08 16:56:05 +0800

    Add products handler class

---
+++ includes/Products.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Products handler.
+ */
+class Products {
+
+
+	/**
+	 * Products handler constructor.
+	 */
+	public function __construct() {
+
+	}
+
+
+}
