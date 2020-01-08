a985f484b7a7c138e75432ba3b86975462e5b23c
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-01-08 22:03:33 +0800

    Add dropdown input field to filter products by status

--- includes/Admin.php
+++ includes/Admin.php
@@ -26,6 +26,9 @@ class Admin {
 		// add column for displaying Facebook sync status
 		add_filter( 'manage_product_posts_columns',       [ $this, 'add_product_list_table_column' ] );
 		add_action( 'manage_product_posts_custom_column', [ $this, 'add_product_list_table_column_content' ] );
+
+		// add input to filter products by Facebook sync status
+		add_action( 'restrict_manage_posts', [ $this, 'add_products_by_sync_status_input_filter' ], 40 );
 	}
 
 
@@ -65,6 +68,30 @@ class Admin {
 				esc_html_e( 'Not synced', 'facebook-for-woocommerce' );
 			}
 		}
+	}
+
+
+	/**
+	 * Adds a dropdown input to let shop managers filter products by sync status.
+	 *
+	 * @internal
+	 */
+	public function add_products_by_sync_status_input_filter() {
+		global $typenow;
+
+		if ( 'product' !== $typenow ) {
+			return;
+		}
+
+		$choice = isset( $_GET['fb_sync_status'] ) ? (string) $_GET['fb_sync_status'] : '';
+
+		?>
+		<select name="fb_sync_status">
+			<option value="" <?php selected( $choice, '' ); ?>><?php esc_html_e( 'Filter by Facebook sync status', 'facebook-for-woocommerce' ); ?></option>
+			<option value="yes" <?php selected( $choice, 'yes' ); ?>><?php esc_html_e( 'Synced to Facebook', 'facebook-for-woocommerce' ); ?></option>
+			<option value="no" <?php selected( $choice, 'no' ); ?>><?php esc_html_e( 'Not synced to Facebook', 'facebook-for-woocommerce' ); ?></option>
+		</select>
+		<?php
 	}
 
 
