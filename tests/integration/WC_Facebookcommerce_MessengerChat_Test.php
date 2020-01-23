c5883e118cb1e46365de5fd0b117f19bf50ab717
Chase Wiseman <chase@skyverge.com>
2020-01-23 14:37:19 -0800

    Add tests for the new messenger chat locales method

---
+++ tests/integration/WC_Facebookcommerce_MessengerChat_Test.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Tests the messenger chat class.
+ */
+class WC_Facebookcommerce_MessengerChat_Test extends \Codeception\TestCase\WPTestCase {
+
+
+	/** @var \IntegrationTester */
+	protected $tester;
+
+
+	/**
+	 * Runs before each test.
+	 */
+	protected function _before() {
+
+
+	}
+
+
+	/**
+	 * Runs after each test.
+	 */
+	protected function _after() {
+
+	}
+
+
+	/** Test methods **************************************************************************************************/
+
+
+	/** @see \WC_Facebookcommerce_MessengerChat::get_supported_locales() */
+	public function test_get_supported_locales() {
+
+		$this->assertIsArray( \WC_Facebookcommerce_MessengerChat::get_supported_locales() );
+	}
+
+
+	/** @see \WC_Facebookcommerce_MessengerChat::get_supported_locales() */
+	public function test_get_supported_locales_filter() {
+
+		add_filter( 'wc_facebook_messenger_supported_locales', function( $locales ) {
+
+			$locales['custom'] = 'Custom';
+
+			return $locales;
+
+		} );
+
+		$this->assertArrayHasKey( 'custom', \WC_Facebookcommerce_MessengerChat::get_supported_locales() );
+	}
+
+
+}
+
