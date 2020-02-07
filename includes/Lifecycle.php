50bc76424fdf451afdfbfd73995accc9eca5897e
Deiva Magalhaes <deiva@skyverge.com>
2020-02-06 17:36:24 -0800

    Fix enable sync setting

--- includes/Lifecycle.php
+++ includes/Lifecycle.php
@@ -84,7 +84,7 @@ class Lifecycle extends Framework\Plugin\Lifecycle {
 		}
 
 		// migrate settings from standalone options
-		$new_settings[ \WC_Facebookcommerce_Integration::SETTING_ENABLE_PRODUCT_SYNC ]      = empty( get_option( 'fb_disable_sync_on_dev_environment', 0 ) );
+		$new_settings[ \WC_Facebookcommerce_Integration::SETTING_ENABLE_PRODUCT_SYNC ]      = empty( get_option( 'fb_disable_sync_on_dev_environment', 0 ) ) ? 'yes' : 'no';
 		$new_settings[ \WC_Facebookcommerce_Integration::SETTING_PRODUCT_DESCRIPTION_MODE ] = ! empty( get_option( 'fb_sync_short_description', 0 ) ) ? \WC_Facebookcommerce_Integration::PRODUCT_DESCRIPTION_MODE_SHORT : \WC_Facebookcommerce_Integration::PRODUCT_DESCRIPTION_MODE_STANDARD;
 
 		$autosync_time = get_option( 'woocommerce_fb_autosync_time' );
