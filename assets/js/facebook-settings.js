27a741ce5bed50b42235b8134753341a6b5761ae
Willington Vega <wvega@wvega.com>
2020-03-11 14:10:07 -0500

    Use jQuery to manipulate sync progress element

--- assets/js/facebook-settings.js
+++ assets/js/facebook-settings.js
@@ -324,9 +324,7 @@ function sync_in_progress() {
 	jQuery( '#woocommerce-facebook-settings-sync-products' ).css( 'pointer-events', 'none' );
 
 	// set products sync status
-	if ( document.querySelector( '#sync_progress' ) ) {
-		document.querySelector( '#sync_progress' ).innerHTML = 'Syncing... Keep this browser open until sync is complete.<span class="spinner is-active"></span>';
-	}
+	jQuery( '#sync_progress' ).show().html( 'Syncing... Keep this browser open until sync is complete.<span class="spinner is-active"></span>' );
 }
 
 
@@ -377,9 +375,7 @@ function sync_not_in_progress(){
 	jQuery( '#woocommerce-facebook-settings-sync-products' ).css( 'pointer-events', 'auto' );
 
 	// Remove sync progress.
-	if (document.querySelector( '#sync_progress' )) {
-		document.querySelector( '#sync_progress' ).innerHTML = '';
-	}
+	jQuery( '#sync_progress' ).empty().hide();
 }
 
 
@@ -653,13 +649,12 @@ function ping_feed_status_queue(count = 0) {
 	);
 }
 
-function product_sync_complete(sync_progress_element) {
+function product_sync_complete( $sync_progress_element ) {
 
 	sync_not_in_progress();
 
-	if (sync_progress_element) {
-		sync_progress_element.innerHTML = '';
-	}
+	$sync_progress_element.empty().hide();
+
 	clearInterval( window.fb_pings );
 }
 
@@ -679,7 +674,9 @@ function check_queues() {
 				clearInterval( window.fb_pings );
 				return;
 			}
-			var sync_progress_element = document.querySelector( '#sync_progress' );
+
+			const $sync_progress_element = jQuery( '#sync_progress' );
+
 			var res = parse_response_check_connection( response );
 			if ( !res ) {
 				if ( fb_sync_no_response_count++ > 5 ) {
@@ -698,11 +695,11 @@ function check_queues() {
 				var processing = !!res.processing; // explicit boolean conversion
 				var remaining  = res.remaining;
 				if ( processing ) {
-					if ( sync_progress_element ) {
-						sync_progress_element.innerHTML = '<strong>Progress:</strong> ' + remaining + ' item' + ( remaining > 1 ? 's' : '' ) + ' remaining.<span class="spinner is-active"></span>';
-					}
+
+					$sync_progress_element.show().html( '<strong>Progress:</strong> ' + remaining + ' item' + ( remaining > 1 ? 's' : '' ) + ' remaining.<span class="spinner is-active"></span>' );
+
 					if ( remaining === 0 ) {
-						product_sync_complete( sync_progress_element );
+						product_sync_complete( $sync_progress_element );
 					}
 				} else {
 					// Not processing, none remaining.  Either long complete, or just completed
@@ -716,7 +713,7 @@ function check_queues() {
 					}
 
 					if ( remaining === 0 ) {
-						  product_sync_complete( sync_progress_element );
+						  product_sync_complete( $sync_progress_element );
 					}
 				}
 			}
@@ -745,8 +742,9 @@ function check_feed_upload_queue(check_num) {
 			"_ajax_nonce": wc_facebook_settings_jsx.nonce,
 		},
 		function(response) {
-			var sync_progress_element = document.querySelector( '#sync_progress' );
-			var res                   = parse_response_check_connection( response );
+			const $sync_progress_element = jQuery( '#sync_progress' );
+
+			var res = parse_response_check_connection( response );
 
 			clearInterval( window.fb_feed_pings );
 
@@ -758,20 +756,19 @@ function check_feed_upload_queue(check_num) {
 						if (window.is_test) {
 							display_test_result();
 						} else {
-							product_sync_complete( sync_progress_element );
+							product_sync_complete( $sync_progress_element );
 						}
 				  break;
 					case 'in progress':
-						if (sync_progress_element) {
-							sync_progress_element.innerHTML = 'Syncing... Keep this browser open until sync is complete.<span class="spinner is-active"></span>';
-						}
+
+						$sync_progress_element.show().html( 'Syncing... Keep this browser open until sync is complete.<span class="spinner is-active"></span>' );
+
 						ping_feed_status_queue( check_num + 1 );
-					  break;
+					break;
 
 					default:
-						if ( sync_progress_element ) {
-							sync_progress_element.innerHTML = '<strong>Something wrong when uploading, please try again.</strong>';
-						}
+
+						$sync_progress_element.show().html( '<strong>Something wrong when uploading, please try again.</strong>' );
 
 						window.feed_upload              = false;
 						if (window.is_test) {
@@ -790,8 +787,9 @@ function display_test_result() {
 			"_ajax_nonce": wc_facebook_settings_jsx.nonce
 		},
 		function(response) {
+			const $sync_progress_element = jQuery( '#sync_progress' );
+
 			var sync_complete_element = document.querySelector( '#sync_complete' );
-			var sync_progress_element = document.querySelector( '#sync_progress' );
 			var res                   = parse_response_check_connection( response );
 			if (res) {
 				var status = res.pass;
@@ -803,18 +801,17 @@ function display_test_result() {
 							sync_complete_element.innerHTML     =
 							'<strong>Status: </strong>Test Pass.';
 						}
-						if (sync_progress_element) {
-							sync_progress_element.innerHTML = '';
-						}
+
+						$sync_progress_element.empty().hide();
+
 						window.is_test = false;
 				  break;
 					case 'in progress':
-						if (sync_progress_element) {
-							sync_progress_element.innerHTML =
-							'<strong>Integration test in progress...</strong>';
-						}
+
+						$sync_progress_element.show().html( '<strong>Integration test in progress...</strong>' );
+
 						ping_feed_status_queue();
-					  break;
+					break;
 					default:
 						window.debug_info = res.debug_info + '<br/>' + res.stack_trace;
 						if (sync_complete_element) {
@@ -822,9 +819,9 @@ function display_test_result() {
 							sync_complete_element.innerHTML     =
 							'<strong>Status: </strong>Test Fail.';
 						}
-						if (sync_progress_element) {
-							sync_progress_element.innerHTML = '';
-						}
+
+						$sync_progress_element.empty().hide();
+
 						if (document.querySelector( '#debug_info' )) {
 							document.querySelector( '#debug_info' ).style.display = '';
 						}
