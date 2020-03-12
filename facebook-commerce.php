fed460b6d9c28825dc911085d5dd84dffc0682f9
Willington Vega <wvega@wvega.com>
2020-03-11 20:39:45 -0500

    Remove duplicate code

--- facebook-commerce.php
+++ facebook-commerce.php
@@ -2004,9 +2004,32 @@ class WC_Facebookcommerce_Integration extends WC_Integration {
 		WC_Facebookcommerce_Utils::check_woo_ajax_permissions( 'syncall products', true );
 		check_ajax_referer( 'wc_facebook_settings_jsx' );
 
+		$this->sync_facebook_products( 'background' );
+	}
+
+
+	/**
+	 * Syncs Facebook products using the GraphAPI.
+	 *
+	 * It can either use a Feed upload or update each product individually based on the selecetd method.
+	 *
+	 * @since 1.10.2-dev.1
+	 *
+	 * @param string $method either 'feed' or 'background'
+	 * @throws Framework\SV_WC_Plugin_Exception
+	 * @return bool
+	 */
+	private function sync_facebook_products( $method ) {
+
+		$sync_started = false;
+
 		try {
 
-			$sync_started = $this->sync_facebook_products_using_background_processor();
+			if ( 'feed' === $method ) {
+				$sync_started = $this->sync_facebook_products_using_feed();
+			} elseif ( 'background' === $method ) {
+				$sync_started = $this->sync_facebook_products_using_background_processor();
+			}
 
 		} catch ( Framework\SV_WC_Plugin_Exception $e ) {
 
@@ -2214,39 +2237,7 @@ class WC_Facebookcommerce_Integration extends WC_Integration {
 		);
 		check_ajax_referer( 'wc_facebook_settings_jsx' );
 
-		try {
-
-			$sync_started = $this->sync_facebook_products_using_feed();
-
-		} catch ( Framework\SV_WC_Plugin_Exception $e ) {
-
-			// Access token has expired
-			if ( 190 === $e->getCode() ) {
-				$error_message = __( 'Your connection has expired.', 'facebook-for-woocommerce' ) . ' <strong>' . __( 'Please click Manage connection > Advanced Options > Update Token to refresh your connection to Facebook.', 'facebook-for-woocommerce' ) . '</strong>';
-			} else {
-				$error_message = $e->getMessage();
-			}
-
-			$sync_started = false;
-		}
-
-		if ( $sync_started ) {
-
-			wp_send_json_success();
-
-		} else {
-
-			if ( isset( $error_message ) ) {
-				$message = sprintf(
-					__( 'There was an error trying to sync the products to Facebook. %s', 'facebook-for-woocommerce' ),
-					$error_message
-				);
-			} else {
-				$message = __( 'There was an error trying to sync the products to Facebook.', 'facebook-for-woocommerce' );
-			}
-
-			wp_send_json_error( [ 'error' => $message ] );
-		}
+		$this->sync_facebook_products( 'feed' );
 	}
 
 
