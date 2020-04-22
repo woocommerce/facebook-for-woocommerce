907cf0299e8b8813f287a0777a580770302e0c4a
Willington Vega <wvega@wvega.com>
2020-04-22 18:45:20 -0500

    Update Feed::get_feed_data_url() to include a 'secret' parameter

--- includes/Products/Feed.php
+++ includes/Products/Feed.php
@@ -210,7 +210,36 @@ class Feed {
 	 */
 	public static function get_feed_data_url() {
 
-		return add_query_arg( 'wc-api', self::REQUEST_FEED_ACTION, home_url( '/' ) );
+		$query_args = [
+			'wc-api' => self::REQUEST_FEED_ACTION,
+			'secret' => self::get_feed_secret(),
+		];
+
+		return add_query_arg( $query_args, home_url( '/' ) );
+	}
+
+
+	/**
+	 * Gets the secret value that should be included in the Feed URL.
+	 *
+	 * Generates a new secret and stores it in the database if no value is set.
+	 *
+	 * @since 1.11.0-dev.1
+	 *
+	 * @return string
+	 */
+	private static function get_feed_secret() {
+
+		$secret = get_option( self::OPTION_FEED_URL_SECRET, '' );
+
+		if  ( ! $secret ) {
+
+			$secret = wp_hash( 'products-feed-' . time() );
+
+			update_option( self::OPTION_FEED_URL_SECRET, $secret );
+		}
+
+		return $secret;
 	}
 
 
