a52778e0bfaa6e09cb846f42b71a2616a2e32740
Deiva Magalhaes <deiva@skyverge.com>
2020-04-24 15:21:32 -0700

    Add tests for Include bulk action for products with an excluded tag

--- tests/acceptance/Admin/ProductSyncBulkActionsCest.php
+++ tests/acceptance/Admin/ProductSyncBulkActionsCest.php
@@ -125,4 +125,48 @@ class ProductSyncBulkActionsCest {
 	}
 
 
+	/**
+	 * Test that the Include in Facebook sync does not enable sync for a product with an excluded tag.
+	 *
+	 * @param AcceptanceTester $I tester instance
+	 *
+	 * @throws Exception
+	 */
+	public function try_include_bulk_action_excluded_tag( AcceptanceTester $I ) {
+
+		// disable sync for the product before viewing the Products page
+		\SkyVerge\WooCommerce\Facebook\Products::disable_sync_for_products( [ $this->product ] );
+
+		// have an excluded tag
+		list( $excluded_tag_id, $excluded_tag_taxonomy_id ) = $I->haveTermInDatabase( 'Excluded tag', 'product_tag' );
+		$I->haveFacebookForWooCommerceSettingsInDatabase( [
+			\WC_Facebookcommerce_Integration::SETTING_EXCLUDED_PRODUCT_TAG_IDS => [ $excluded_tag_id ]
+		] );
+
+		// add the excluded tag to the product
+		wp_add_object_terms( $this->product->get_id(), [ $excluded_tag_id ], 'product_tag' );
+
+		$I->amOnProductsPage();
+
+		$I->see( 'Excluded tag', 'table.wp-list-table td' );
+		$I->see( 'Disabled', 'table.wp-list-table td' );
+
+		$I->wantTo( 'Test that the Include in Facebook sync does not enable sync for a product with an excluded tag' );
+
+		$I->click( "#cb-select-{$this->product->get_id()}" );
+		$I->selectOption( '[name=action]', 'Include in Facebook sync' );
+		$I->click( '#doaction' );
+
+		$I->waitForElementVisible( '#wc-backbone-modal-dialog' );
+		$I->see( 'One or more of the selected products belongs to a category or tag that is excluded from the Facebook catalog sync. To sync these products to Facebook, please remove the category or tag exclusion from the plugin settings.', '#wc-backbone-modal-dialog' );
+		$I->see( 'Go to Settings', '#wc-backbone-modal-dialog' );
+		$I->see( 'Cancel', '#wc-backbone-modal-dialog' );
+
+		$I->click( 'Cancel', '#wc-backbone-modal-dialog' );
+
+		$I->waitForElementNotVisible( '#wc-backbone-modal-dialog' );
+		$I->see( 'Disabled', 'table.wp-list-table td' );
+	}
+
+
 }
