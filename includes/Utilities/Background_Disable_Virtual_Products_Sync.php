a8aa8d64be38d7c73fe5a925b04945ca19895a62
Deiva Magalhaes <deiva@skyverge.com>
2020-04-24 16:03:16 -0700

    Add background job handler to disable sync for virtual products

---
+++ includes/Utilities/Background_Disable_Virtual_Products_Sync.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\Utilities;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\PluginFramework\v5_5_4 as Framework;
+
+
+/**
+ * Background job handler to exclude virtual products from sync.
+ *
+ * @since 1.11.1-dev.1
+ */
+class Background_Disable_Virtual_Products_Sync extends Framework\SV_WP_Background_Job_Handler {
+
+
+	/**
+	 * Background job constructor.
+	 *
+	 * @since 1.11.1-dev.1
+	 */
+	public function __construct() {
+
+		$this->prefix = 'wc_facebook';
+		$this->action = 'background_disable_virtual_products_sync';
+
+		parent::__construct();
+	}
+
+
+	/**
+	 * Processes job.
+	 *
+	 * This job continues to update products meta data until we run out of memory or exceed the time limit.
+	 * There is no list of items to loop over.
+	 *
+	 * @since 1.11.1-dev.1
+	 *
+	 * @param object $job
+	 * @param int $items_per_batch number of items to process in a single request. Defaults to unlimited.
+	 * @return object
+	 */
+	public function process_job( $job, $items_per_batch = null ) {
+
+		if ( ! isset( $job->total ) ) {
+			$job->total = $this->count_remaining_products();
+		}
+
+		if ( ! isset( $job->progress ) ) {
+			$job->progress = 0;
+		}
+
+		$remaining_products = $job->total;
+		$processed_products = 0;
+
+		// disable sync until memory or time limit is exceeded
+		while ( $processed_products < $remaining_products ) {
+
+			$rows_inserted = $this->disable_sync();
+
+			$processed_products += $rows_inserted;
+			$job->progress     += $rows_inserted;
+
+			// update job progress
+			$job = $this->update_job( $job );
+
+			// memory or time limit reached
+			if ( $this->time_exceeded() || $this->memory_exceeded() ) {
+				break;
+			}
+		}
+
+		// job complete! :)
+		if ( $this->count_remaining_products() === 0 ) {
+
+			update_option( 'wc_facebook_sync_virtual_products_disabled', 'yes' );
+
+			$this->complete_job( $job );
+		}
+
+		return $job;
+	}
+
+
+	/**
+	 * Counts the number of virtual products with sync enabled.
+	 *
+	 * @since 1.11.1-dev.1
+	 *
+	 * @return bool
+	 */
+	private function count_remaining_products() {
+		global $wpdb;
+
+		$sql = "
+			SELECT COUNT( posts.ID )
+			FROM {$wpdb->posts} AS posts
+			INNER JOIN {$wpdb->postmeta} AS sync_meta ON ( posts.ID = sync_meta.post_id AND sync_meta.meta_key = '_wc_facebook_sync_enabled' AND sync_meta.meta_value = 'yes' )
+			INNER JOIN {$wpdb->postmeta} AS virtual_meta ON ( posts.ID = virtual_meta.post_id AND virtual_meta.meta_key = '_virtual' AND virtual_meta.meta_value = 'yes' )
+			WHERE posts.post_type = 'product'
+		";
+
+		return (int) $wpdb->get_var( $sql );
+	}
+
+
+	/**
+	 * Update rows into the postmeta table to disable sync.
+	 *
+	 * @since 1.11.1-dev.1
+	 *
+	 * @return int
+	 */
+	private function disable_sync() {
+		global $wpdb;
+
+		$sql = "
+			UPDATE {$wpdb->postmeta} AS sync_meta
+				SET meta_value = 'no'
+				INNER JOIN {$wpdb->posts} AS posts ON ( posts.ID = sync_meta.post_id AND sync_meta.meta_key = '_wc_facebook_sync_enabled' AND sync_meta.meta_value = 'yes' )
+				INNER JOIN {$wpdb->postmeta} AS virtual_meta ON ( posts.ID = virtual_meta.post_id AND virtual_meta.meta_key = '_virtual' AND virtual_meta.meta_value = 'yes' )
+				WHERE posts.post_type = 'product'
+				LIMIT 1000
+		";
+
+		$rows_updated = $wpdb->query( $sql );
+
+		if ( false === $rows_updated ) {
+
+			$message = sprintf( 'There was an error trying to update products meta data. %s', $wpdb->last_error );
+
+			facebook_for_woocommerce()->log( $message );
+		}
+
+		return (int) $rows_updated;
+	}
+
+
+	/**
+	 * No-op
+	 *
+	 * @since 1.11.1-dev.1
+	 */
+	protected function process_item( $item, $job ) {
+		// void
+	}
+
+
+}
