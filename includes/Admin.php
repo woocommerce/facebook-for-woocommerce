03d1392ebb91d2945ef5b7136fa8160611833806
Deiva Magalhaes <deiva@skyverge.com>
2020-04-24 10:59:47 -0700

    Hide Facebook tab from product edit screen if virtual

--- includes/Admin.php
+++ includes/Admin.php
@@ -657,7 +657,7 @@ class Admin {
 		$tabs['fb_commerce_tab'] = [
 			'label'  => __( 'Facebook', 'facebook-for-woocommerce' ),
 			'target' => 'facebook_options',
-			'class'  => [ 'show_if_simple' ],
+			'class'  => [ 'show_if_simple', 'hide_if_virtual' ],
 		];
 
 		return $tabs;
