c5e0bb1118e0729f801909e1b5f97c415b2bfbdf
Willington Vega <wvega@wvega.com>
2020-05-11 18:50:13 -0500

    Add Handlers\Connection class

---
+++ includes/Handlers/Connection.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\Handlers;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\PluginFramework\v5_5_4 as Framework;
+
+/**
+ * The connection handler.
+ *
+ * @since 2.0.0-dev.1
+ */
+class Connection {
+
+
+}
