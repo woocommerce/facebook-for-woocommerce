208829e0f7b1e94a5bf929e9d9f0475017b984bd
Willington Vega <wvega@wvega.com>
2020-05-13 22:38:50 -0500

    Add acceptance tests for the Product Metabox

---
+++ tests/acceptance/Admin/ProductMetaboxCest.php
@@ -0,0 +1,65 @@
+<?php
+
+use SkyVerge\WooCommerce\Facebook\Handlers\Connection;
+
+class ProductMetaboxCest {
+
+
+	/** @var WC_Product|null product object created for the test */
+	private $product;
+
+
+	/**
+	 * Runs before each test.
+	 *
+	 * @param AcceptanceTester $I tester instance
+	 * @throws \Exception
+	 */
+    public function _before( AcceptanceTester $I ) {
+
+		$this->product = $I->haveProductInDatabase();
+
+		// always log in
+		$I->loginAsAdmin();
+    }
+
+
+	/**
+	 * Tests that the field is unchecked if sync is disabled in parent.
+	 *
+	 * @param AcceptanceTester $I
+	 * @throws \Exception
+	 */
+	public function try_metabox_is_not_visibile_if_the_plugin_is_not_connected( AcceptanceTester $I ) {
+
+		$I->wantTo( 'Test that product metabox is not shown when the plugin is connected' );
+
+		$I->dontHaveOptionInDatabase( Connection::OPTION_ACCESS_TOKEN );
+
+		$I->amEditingPostWithId( $this->product->get_id() );
+		$I->waitForElementVisible( 'input[type="submit"][value="Update"]', 5 );
+
+		$I->dontSeeElement( [ 'css' => '#facebook_metabox' ] );
+	}
+
+
+	/**
+	 * Tests that the field is unchecked if sync is disabled in parent.
+	 *
+	 * @param AcceptanceTester $I
+	 * @throws \Exception
+	 */
+	public function try_metabox_is_visibile_if_the_plugin_is_connected( AcceptanceTester $I ) {
+
+		$I->wantTo( 'Test that product metabox is shown when the plugin is connected' );
+
+		$I->haveOptionInDatabase( Connection::OPTION_ACCESS_TOKEN, 'xyz' );
+
+		$I->amEditingPostWithId( $this->product->get_id() );
+		$I->waitForElementVisible( 'input[type="submit"][value="Update"]', 5 );
+
+		$I->seeElement( [ 'css' => '#facebook_metabox' ] );
+	}
+
+
+}
