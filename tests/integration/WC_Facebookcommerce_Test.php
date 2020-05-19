68a83d24cfa52421d82957daa67e6d0df03c419e
Jared Burke <jdeeburke@gmail.com>
2020-05-19 17:22:18 -0500

    Add tests for new handlers

--- tests/integration/WC_Facebookcommerce_Test.php
+++ tests/integration/WC_Facebookcommerce_Test.php
@@ -1,6 +1,8 @@
 <?php
 
 use SkyVerge\WooCommerce\Facebook\Handlers\Connection;
+use SkyVerge\WooCommerce\Facebook\Products\Sync;
+use SkyVerge\WooCommerce\Facebook\Products\Sync\Background;
 
 /**
  * Tests the WC_Facebookcommerce class.
@@ -19,6 +21,20 @@ class WC_Facebookcommerce_Test extends \Codeception\TestCase\WPTestCase {
 	public function test_get_connection_handler() {
 
 		$this->assertInstanceOf( Connection::class, facebook_for_woocommerce()->get_connection_handler() );
+	}
+
+
+	/** @see WC_Facebookcommerce::get_products_sync_handler() */
+	public function test_get_products_sync_handler() {
+
+		$this->assertInstanceOf( Sync::class, facebook_for_woocommerce()->get_products_sync_handler() );
+	}
+
+
+	/** @see WC_Facebookcommerce::get_products_sync_background_handler() */
+	public function test_get_products_sync_background_handler() {
+
+		$this->assertInstanceOf( Background::class, facebook_for_woocommerce()->get_products_sync_handler() );
 	}
 
 
