dadcaab5b1cd797ff4fd8d8609c025057e137a8b
Deiva Magalhaes <deiva@skyverge.com>
2020-05-19 16:08:29 -0700

    Create API class

---
+++ includes/API.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook;
+
+use SkyVerge\WooCommerce\PluginFramework\v5_5_4\SV_WC_API_Base;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * API handler.
+ *
+ * @since 2.0.0-dev.1
+ */
+class API extends SV_WC_API_Base {
+
+
+}
