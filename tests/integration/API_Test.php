30e3a638541a7e42bb507a1606a86a823f5f366f
Deiva Magalhaes <deiva@skyverge.com>
2020-05-19 16:30:56 -0700

    Add test class

---
+++ tests/integration/API_Test.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Tests the API class.
+ */
+class API_Test extends \Codeception\TestCase\WPTestCase {
+
+
+	/** @var \IntegrationTester */
+	protected $tester;
+
+
+	/**
+	 * Runs before each test.
+	 */
+	protected function _before() {
+
+	}
+
+
+	/**
+	 * Runs after each test.
+	 */
+	protected function _after() {
+
+	}
+
+
+	/** Test methods **************************************************************************************************/
+
+
+}
