94ba7667a35b09becad15ad7d338812184039e88
Deiva Magalhaes <deiva@skyverge.com>
2020-05-19 14:29:07 -0700

    Add test for create_or_update_products

--- tests/integration/Products/SyncTest.php
+++ tests/integration/Products/SyncTest.php
@@ -12,11 +12,55 @@ class SyncTest extends \Codeception\TestCase\WPTestCase {
 	protected $tester;
 
 
+	public function _before() {
+
+		parent::_before();
+
+		require_once 'includes/Products/Sync.php';
+	}
+
+
 	/** Test methods **************************************************************************************************/
+
+
+	/** @see Sync::create_or_update_products() */
+	public function test_create_or_update_products() {
+
+		$product_ids = [ 123, 456 ];
+
+		$sync = $this->get_sync();
+
+		$sync->create_or_update_products( $product_ids );
+
+		$requests_property = new \ReflectionProperty( Sync::class, 'requests' );
+		$requests_property->setAccessible( true );
+
+		$requests = $requests_property->getValue( $sync );
+
+		$this->assertIsArray( $requests );
+		$this->assertArrayHasKey( 'p-123', $requests );
+		$this->assertArrayHasKey( 'p-456', $requests );
+		$this->assertEquals( 'UPDATE', $requests['p-123'] );
+		$this->assertEquals( 'UPDATE', $requests['p-456'] );
+	}
 
 
 	/** @see Sync::delete_products() */
 	public function test_delete_products() {
+	}
+
+
+	/** Helper methods **************************************************************************************************/
+
+
+	/**
+	 * Gets the handler instance.
+	 *
+	 * @return Sync
+	 */
+	private function get_sync() {
+
+		return new Sync();
 	}
 
 
