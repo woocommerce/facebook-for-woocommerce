17fa234d8e268a8307bc7802d860a9b172aacd64
Willington Vega <wvega@wvega.com>
2020-05-19 16:12:21 -0500

    Add empty integration test for Sync class

---
+++ tests/integration/Products/SyncTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use SkyVerge\WooCommerce\Facebook\Products\Sync;
+
+/**
+ * Tests the Feed class.
+ */
+class SyncTest extends \Codeception\TestCase\WPTestCase {
+
+
+	/** @var \IntegrationTester */
+	protected $tester;
+
+
+	/** Test methods **************************************************************************************************/
+
+
+	/** @see Sync::delete_products() */
+	public function test_delete_products() {
+	}
+
+
+}
+
