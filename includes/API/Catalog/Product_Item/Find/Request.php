829c43cbc29dc0c8761b03e873783d2d9a1ad915
Deiva Magalhaes <deiva@skyverge.com>
2020-05-20 11:37:14 -0700

    Add Find\Request class

---
+++ includes/API/Catalog/Product_Item/Find/Request.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Catalog\Product_Item\Find;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Find Product Item API request object.
+ *
+ * @since 2.0.0-dev.1
+ */
+class Request extends \SkyVerge\WooCommerce\Facebook\API\Request {
+
+
+}
