673999aa18b231b81699500d4133a52f718f204b
Willington Vega <wvega@wvega.com>
2020-05-20 13:53:36 -0500

    Add tests for API\Request class

---
+++ tests/integration/API/RequestTest.php
@@ -0,0 +1,69 @@
+<?php
+
+use SkyVerge\WooCommerce\Facebook\API\Request;
+
+/**
+ * Tests the API\Request class.
+ */
+class RequestTest extends \Codeception\TestCase\WPTestCase {
+
+
+	/** @var \IntegrationTester */
+	protected $tester;
+
+
+	public function _before() {
+
+		parent::_before();
+
+		require_once 'includes/API/Request.php';
+	}
+
+
+	/** Test methods **************************************************************************************************/
+
+
+	/**
+	 * @see Request::__construct()
+	 *
+	 * @param string $object_id object ID
+	 * @param string $path endpoint route
+	 * @param string $method HTTP method
+	 *
+	 * @dataProvider provider_constructor
+	 */
+	public function test_constructor( $object_id, $path, $method, $expect_path ) {
+
+		$request = new Request( $object_id, $path, $method );
+
+		$this->assertEquals( $expect_path, $request->get_path() );
+		$this->assertEquals( $method, $request->get_method() );
+	}
+
+	/** @see test_constructor */
+	public function provider_constructor( $requests ) {
+
+		return [
+			[ 'me', '', 'GET', '/me' ],
+			[ '1234', '/products', 'GET', '/1234/products' ],
+
+			[ '1234', 'batch', 'POST', '/1234/batch' ],
+			[ '1234', '/batch/', 'POST', '/1234/batch' ],
+			[ '1234', '', 'POST', '/1234' ],
+		];
+	}
+
+
+	/** @see Request::set_data() */
+	public function test_set_data() {
+
+		$request = new Request( null, null, null );
+		$data    = [ 'key' => 'value' ];
+
+		$request->set_data( $data );
+
+		$this->assertEquals( $data, $request->get_data() );
+	}
+
+
+}
