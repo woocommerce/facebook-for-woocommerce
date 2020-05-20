61cf173273e220c6c69be89355ac2c7944a16d04
Deiva Magalhaes <deiva@skyverge.com>
2020-05-20 12:02:24 -0700

    Add tests for Find request

---
+++ tests/integration/API/Catalog/Product_Item/Find/Request_Test.php
@@ -0,0 +1,50 @@
+<?php
+
+use SkyVerge\WooCommerce\Facebook\API\Catalog\Product_Item\Find\Request;
+
+/**
+ * Tests the Find Product Item Request class.
+ */
+class Request_Test extends \Codeception\TestCase\WPTestCase {
+
+
+	/** @var \IntegrationTester */
+	protected $tester;
+
+
+	/**
+	 * Runs before each test.
+	 */
+	protected function _before() {
+
+		require_once 'includes/API/Request.php';
+		require_once 'includes/API/Catalog/Product_Item/Find/Request.php';
+	}
+
+
+	/**
+	 * Runs after each test.
+	 */
+	protected function _after() {
+
+	}
+
+
+	/** Test methods **************************************************************************************************/
+
+
+	/** @see \SkyVerge\WooCommerce\Facebook\API\Catalog\Product_Item\Find\Request::__construct */
+	public function test_constructor() {
+
+		$catalog_id  = '165835951532406';
+		$retailer_id = base64_decode( 'd3BfcG9zdF8xMDQx' );
+
+		$request = new Request( $catalog_id, $retailer_id );
+
+		$this->assertInstanceOf( Request::class, $request );
+		$this->assertEquals( 'catalog:165835951532406:d3BfcG9zdF8xMDQx', $request->get_path() );
+		$this->assertEquals( 'GET', $request->get_method() );
+	}
+
+
+}
