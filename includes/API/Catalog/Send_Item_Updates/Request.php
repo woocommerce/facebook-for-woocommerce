2d9ddac07734d2bcd019b557c6d74f897ea37cf4
Willington Vega <wvega@wvega.com>
2020-05-20 13:50:19 -0500

    Create the API\Catalog\Send_Item_Updates\Request class

---
+++ includes/API/Catalog/Send_Item_Updates/Request.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Catalog\Send_Item_Updates;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\Facebook\API;
+
+/**
+ * Base API request object.
+ *
+ * @since 2.0.0-dev.1
+ */
+class Request extends API\Request  {
+
+}
