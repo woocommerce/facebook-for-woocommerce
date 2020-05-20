e4f227e6f0c1f761c81b68f62072149b7a0550f4
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-05-20 16:55:17 +0800

    Update support URL

--- tests/integration/WC_Facebookcommerce_Test.php
+++ tests/integration/WC_Facebookcommerce_Test.php
@@ -15,10 +15,17 @@ class WC_Facebookcommerce_Test extends \Codeception\TestCase\WPTestCase {
 	/** Test methods **************************************************************************************************/
 
 
-	/** @see WC_Facebookcommerce::get_connection_handler() */
+	/** @see \WC_Facebookcommerce::get_connection_handler() */
 	public function test_get_connection_handler() {
 
 		$this->assertInstanceOf( Connection::class, facebook_for_woocommerce()->get_connection_handler() );
+	}
+
+
+	/** @see \WC_Facebookcommerce::get_support_url() */
+	public function test_get_support_url() {
+
+		$this->assertEquals( 'https://wordpress.org/support/plugin/facebook-for-woocommerce/', facebook_for_woocommerce()->get_support_url() );
 	}
 
 
