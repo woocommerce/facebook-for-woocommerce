23034ec2a7d0d9f3a4f720f5433cf762b03015e4
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-05-20 18:32:52 +0800

    Add base API request class

---
+++ includes/API/Request.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\PluginFramework\v5_5_4 as Framework;
+
+/**
+ * Base API request object.
+ *
+ * @since 2.0.0-dev.1
+ */
+class Request extends Framework\SV_WC_API_JSON_Request {
+
+
+}
