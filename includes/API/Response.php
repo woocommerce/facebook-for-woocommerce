0c4f3573ca7b79d8d0705342140be3cf1674f1b5
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-05-20 18:42:13 +0800

    Add API response class

---
+++ includes/API/Response.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\PluginFramework\v5_5_4 as Framework;
+
+class Response extends Framework\SV_WC_API_JSON_Response {
+
+
+}
