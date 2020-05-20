2754050cccab0d8087fa6f0bda724f7acfda9d21
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-05-20 18:43:56 +0800

    Update API response phpdoc

--- includes/API/Response.php
+++ includes/API/Response.php
@@ -14,6 +14,11 @@ defined( 'ABSPATH' ) or exit;
 
 use SkyVerge\WooCommerce\PluginFramework\v5_5_4 as Framework;
 
+/**
+ * Base API response object
+ *
+ * @since 2.0.0-dev.1
+ */
 class Response extends Framework\SV_WC_API_JSON_Response {
 
 
