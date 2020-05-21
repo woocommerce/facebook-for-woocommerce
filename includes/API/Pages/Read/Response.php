584449d237ad8984b0d3141788b6e6b60c666b04
Willington Vega <wvega@wvega.com>
2020-05-21 09:36:56 -0500

    Add the API\Pages\Read\Response class

---
+++ includes/API/Pages/Read/Response.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Pages\Read;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\Facebook\API;
+
+/**
+ * Page API response object.
+ *
+ * @since 2.0.0-dev.1
+ */
+class Response extends API\Response  {
+
+
+}
