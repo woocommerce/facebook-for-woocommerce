aa524ecb9d92eea6b867a98eaf588983ebb4e7e6
Willington Vega <wvega@wvega.com>
2020-05-20 19:43:22 -0500

    Add tests for API\Catalog\Product_Item\Response::get_group_id()

---
+++ tests/integration/API/Catalog/Product_Item/ResponseTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SkyVerge\WooCommerce\Facebook\Tests\API\Catalog\Product_Item;
+
+use SkyVerge\WooCommerce\Facebook\API\Catalog\Product_Item\Response;
+
+/**
+ * Tests the Connection class.
+ */
+class ResponseTest extends \Codeception\TestCase\WPTestCase {
+
+
+	/** @var \IntegrationTester */
+	protected $tester;
+
+
+	public function _before() {
+
+		parent::_before();
+
+		require_once 'includes/API/Catalog/Product_Item/Response.php';
+	}
+
+
+	/** Test methods **************************************************************************************************/
+
+
+	/**
+	 * @see Response::get_group_id()
+	 *
+	 * @param string $raw_response JSON encoded response
+	 * @param string $product_group_id expected product group ID
+	 *
+	 * @dataProvider provider_get_group_id()
+	 */
+	public function test_get_group_id( $raw_response, $product_group_id ) {
+
+		$response = new Response( $raw_response );
+
+		$this->assertEquals( $product_group_id, $response->get_group_id() );
+	}
+
+
+	public function provider_get_group_id() {
+
+		return [
+			[ json_encode( [ 'product_group' => [ 'id' => '1234' ] ] ), '1234' ],
+			[ json_encode( [ 'id' => '1234' ] ),                        '' ],
+		];
+	}
+
+
+}
