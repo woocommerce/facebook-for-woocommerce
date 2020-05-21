403d691e3ea50df767c4ac7e2f7d859e3aa921a8
Willington Vega <wvega@wvega.com>
2020-05-20 19:42:36 -0500

    Add API\Catalog\Product_Item\Response class

---
+++ includes/API/Catalog/Product_Item/Response.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Catalog\Product_Item;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\PluginFramework\v5_5_4 as Framework;
+
+/**
+ * Response object for API requests that return a Product Item.
+ *
+ * @since 2.0.0-dev.1
+ */
+class Response extends Framework\SV_WC_API_JSON_Response {
+
+
+	/**
+	 * Gets the Product Item group ID.
+	 *
+	 * @since 2.0.0-dev.1
+	 *
+	 * @return string
+	 */
+	public function get_group_id() {
+
+		$product_group = '';
+
+		if ( isset( $this->response_data->product_group->id ) ) {
+			$product_group = $this->response_data->product_group->id;
+		}
+
+		return $product_group;
+	}
+
+
+}
