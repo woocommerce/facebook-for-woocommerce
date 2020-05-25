f4872af82a67f169fe952089ceef0ede45d254ef
Chase Wiseman <chase@skyverge.com>
2020-05-24 19:33:48 -0700

    Render the Facebook box

--- includes/Admin/Settings_Screens/Connection.php
+++ includes/Admin/Settings_Screens/Connection.php
@@ -39,6 +39,119 @@ class Connection extends Admin\Abstract_Settings_Screen {
 
 
 	/**
+	 * Renders the screen.
+	 *
+	 * @since 2.0.0-dev.1
+	 */
+	public function render() {
+
+		$is_connected = facebook_for_woocommerce()->get_connection_handler()->is_connected();
+
+		// always render the CTA box
+		$this->render_facebook_box( $is_connected );
+
+		// don't proceed further if not connected
+		if ( ! $is_connected ) {
+			return;
+		}
+
+		parent::render();
+	}
+
+
+	/**
+	 * Renders the Facebook CTA box.
+	 *
+	 * @since 2.0.0-dev.1
+	 *
+	 * @param bool $is_connected whether the plugin is connected
+	 */
+	public function render_facebook_box( $is_connected ) {
+
+		if ( $is_connected ) {
+			$title = __( 'Reach the Right People and Sell More Online', 'facebook-for-woocommerce' );
+		} else {
+			$title = __( 'Grow your business on Facebook', 'facebook-for-woocommerce' );
+		}
+
+		$subtitle = __( 'Use this WooCommerce and Facebook integration to:', 'facebook-for-woocommerce' );
+
+		$benefits = [
+			__( 'Create an ad in a few steps', 'facebook-for-woocommerce'),
+			__( 'Use built-in best practices for online sales', 'facebook-for-woocommerce'),
+			__( 'Get reporting on sales and revenue', 'facebook-for-woocommerce'),
+		];
+
+		if ( $is_connected ) {
+
+			$actions = [
+				'create-ad' => [
+					'label' => __( 'Create Ad', 'facebook-for-woocommerce' ),
+					'type'  => 'primary',
+					'url'   => 'https://www.facebook.com/ad_center/create/ad/?entry_point=facebook_ads_extension&page_id=' . facebook_for_woocommerce()->get_integration()->get_facebook_page_id(),
+				],
+				'manage' => [
+					'label' => __( 'Manage', 'facebook-for-woocommerce' ),
+					'type'  => 'secondary',
+					'url'   => facebook_for_woocommerce()->get_connection_handler()->get_manage_url(),
+				],
+			];
+
+		} else {
+
+			$actions = [
+				'get-started' => [
+					'label' => __( 'Get Started', 'facebook-for-woocommerce' ),
+					'type'  => 'primary',
+					'url'   => facebook_for_woocommerce()->get_connection_handler()->get_connect_url(),
+				],
+			];
+		}
+
+		?>
+
+		<div id="wc-facebook-connection-box">
+
+			<h1><?php echo esc_html( $title ); ?></h1>
+			<h2><?php echo esc_html( $subtitle ); ?></h2>
+
+			<ul class="benefits">
+				<?php foreach ( $benefits as $benefit ) : ?>
+					<li><?php echo esc_html( $benefit ); ?></li>
+				<?php endforeach; ?>
+			</ul>
+
+			<div class="actions">
+
+				<?php foreach ( $actions as $action ) : ?>
+
+					<a
+						href="<?php echo esc_url( $action['url'] ); ?>"
+						class="<?php echo ( 'internal' !== $action['type'] ) ? 'button' : ''; ?> button-<?php echo esc_attr( $action['type'] ); ?>"
+						<?php echo ( 'internal' !== $action['type'] ) ? 'target="_blank"' : ''; ?>
+					>
+						<?php echo esc_html( $action['label'] ); ?>
+					</a>
+
+				<?php endforeach; ?>
+
+				<?php if ( $is_connected ) : ?>
+
+					<a href="<?php echo esc_url( facebook_for_woocommerce()->get_connection_handler()->get_disconnect_url() ); ?>" class="uninstall">
+						<?php esc_html_e( 'Uninstall', 'facebook-for-woocommerce' ); ?>
+					</a>
+
+				<?php endif; ?>
+
+			</div>
+
+		</div>
+
+		<?php
+	}
+
+
+	/**
 	 * Gets the screen settings.
 	 *
 	 * @since 2.0.0-dev.1
