600a106afc517dbac70f244595c737f8d50e13d6
Willington Vega <wvega@wvega.com>
2020-05-25 10:11:08 -0500

    Update Background::process_item_update() to receive a prefixed product ID

--- includes/Products/Sync/Background.php
+++ includes/Products/Sync/Background.php
@@ -196,11 +196,18 @@ class Background extends Framework\SV_WP_Background_Job_Handler {
 	 *
 	 * @since 2.0.0-dev.1
 	 *
-	 * @param \WC_Product $product product object
+	 * @param string $prefixed_product_id prefixed product ID
 	 * @return array
 	 * @throws Framework\SV_WC_Plugin_Exception
 	 */
-	private function process_item_update( $product ) {
+	private function process_item_update( $prefixed_product_id ) {
+
+		$product_id = (int) str_replace( Sync::PRODUCT_INDEX_PREFIX, '', $prefixed_product_id );
+		$product    = wc_get_product( $product_id );
+
+		if ( ! $product instanceof \WC_Product ) {
+			throw new Framework\SV_WC_Plugin_Exception( "No product found with ID equal to {$product_id}." );
+		}
 
 		if ( $product->is_type( 'variation' ) ) {
 			$product_data = $this->prepare_product_variation_data( $product );
