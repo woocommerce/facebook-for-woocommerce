1ac82c2a1192c099ea94979fdfeca4bcdb0d77b6
Deiva Magalhaes <deiva@skyverge.com>
2020-05-26 14:11:15 -0700

    Add rate limited response trait

---
+++ includes/API/Traits/Rate_Limited_Response.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Traits;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Rate limited response trait.
+ *
+ * @since 2.0.0-dev.1
+ */
+trait Rate_Limited_Response {
+
+
+}
