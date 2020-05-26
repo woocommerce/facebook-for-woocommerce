95f7b7d8d967d9e66ca0c0f66cf14c9d48b2cd42
Deiva Magalhaes <deiva@skyverge.com>
2020-05-26 16:33:37 -0700

    Define Rate_Limited_API::set_rate_limit_delay()

--- includes/API/Traits/Rate_Limited_API.php
+++ includes/API/Traits/Rate_Limited_API.php
@@ -20,4 +20,18 @@ defined( 'ABSPATH' ) or exit;
 trait Rate_Limited_API {
 
 
+	/**
+	 * Stores the delay, in seconds, for requests with the given rate limit ID.
+	 *
+	 * @since 2.0.0-dev.1
+	 *
+	 * @param string $rate_limit_id request ID for rate limiting
+	 * @param int $delay delay in seconds
+	 */
+	public function set_rate_limit_delay( $rate_limit_id, $delay ) {
+
+		update_option( "wc_facebook_rate_limit_${rate_limit_id}", $delay );
+	}
+
+
 }
