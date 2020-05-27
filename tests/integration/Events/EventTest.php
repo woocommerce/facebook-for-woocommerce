7b922b0a3f7982c7bd81fd798943f90505463824
Deiva Magalhaes <deiva@skyverge.com>
2020-05-26 17:53:25 -0700

    Add EventTest class

---
+++ tests/integration/Events/EventTest.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace SkyVerge\WooCommerce\Facebook\Events;
+
+/**
+ * Tests the Event class.
+ */
+class EventTest extends \Codeception\TestCase\WPTestCase {
+
+
+	/** @var \IntegrationTester */
+	protected $tester;
+
+
+	/**
+	 * Runs before each test.
+	 */
+	protected function _before() {
+
+		parent::_before();
+	}
+
+
+	/**
+	 * Runs after each test.
+	 */
+	protected function _after() {
+
+	}
+
+
+	/** Test methods **************************************************************************************************/
+
+
+	/** @see Event::__construct() */
+	public function test_constructor() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::prepare_data() */
+	public function test_prepare_data() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::prepare_user_data() */
+	public function test_prepare_user_data() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::generate_event_id() */
+	public function test_generate_event_id() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::get_current_url() */
+	public function test_get_current_url() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::get_client_ip() */
+	public function test_get_client_ip() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::get_client_user_agent() */
+	public function test_get_client_user_agent() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::get_click_id() */
+	public function test_get_click_id() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::get_browser_id() */
+	public function test_get_browser_id() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::get_data() */
+	public function test_get_data() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::get_id() */
+	public function test_get_id() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::get_name() */
+	public function test_get_name() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::get_user_data() */
+	public function test_get_user_data() {
+
+		// TODO: implement
+	}
+
+
+	/** @see Event::get_custom_data() */
+	public function test_get_custom_data() {
+
+		// TODO: implement
+	}
+
+
+}
