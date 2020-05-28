6929fb300394235d03c4e9ccd4e1dfa38ab476d8
José Antonio Jiménez <josejia@fb.com>
2020-05-28 16:19:35 -0700

    Updating the plugin to send AddToCart event when AJAX is used
    Summary: The function send_server_events is hooked to the action woocommerce_ajax_added_to_cart in order to send AddToCart events when an item is added via AJAX.
    
    Reviewed By: vinothsa
    
    Differential Revision: D21774898
    
    fbshipit-source-id: 8533212d

--- facebook-commerce-events-tracker.php
+++ facebook-commerce-events-tracker.php
@@ -59,6 +59,7 @@ if ( ! class_exists( 'WC_Facebookcommerce_EventsTracker' ) ) :
 			add_action( 'woocommerce_add_to_cart', [ $this, 'inject_add_to_cart_event' ], 40, 4 );
 			// AddToCart while AJAX is enabled
 			add_action( 'woocommerce_ajax_added_to_cart', [ $this, 'add_filter_for_add_to_cart_fragments' ] );
+			add_action( 'woocommerce_ajax_added_to_cart', [ $this, 'send_tracked_events' ], 10, 2 );
 			// AddToCart while using redirect to cart page
 			if ( 'yes' === get_option( 'woocommerce_cart_redirect_after_add' ) ) {
 				add_filter( 'woocommerce_add_to_cart_redirect', [ $this, 'set_last_product_added_to_cart_upon_redirect' ], 10, 2 );
