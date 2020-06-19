27540c2f08dec7228c0d37c914b937fbd30a8168
Chase Wiseman <chase@skyverge.com>
2020-06-19 13:50:30 -0700

    Add a notice for upgraders about the new settings location

--- class-wc-facebookcommerce.php
+++ class-wc-facebookcommerce.php
@@ -189,7 +189,25 @@ if ( ! class_exists( 'WC_Facebookcommerce' ) ) :
 						'<a href="' . esc_url( $this->get_connection_handler()->get_connect_url() ) . '">', '</a>'
 					);
 
-					$message_id = 'migrate_to_v2_0';
+					$this->get_admin_notice_handler()->add_admin_notice( $message, self::PLUGIN_ID . '_migrate_to_v2_0', [
+						'dismissible'  => false,
+						'notice_class' => 'notice-info',
+					] );
+
+					// direct these users to the new plugin settings page
+					if ( ! $this->is_plugin_settings() ) {
+
+						$message = sprintf(
+							/* translators: Placeholders %1$s - opening link HTML tag, %2$s - closing link HTML tag */
+							__( 'For your convenience, the Facebook for WooCommerce settings are now located under %1$sWooCommerce > Facebook%2$s.', 'facebook-for-woocommerce' ),
+							'<a href="' . esc_url( facebook_for_woocommerce()->get_settings_url() ) . '">', '</a>'
+						);
+
+						$this->get_admin_notice_handler()->add_admin_notice( $message, self::PLUGIN_ID . '_relocated_settings', [
+							'dismissible'  => true,
+							'notice_class' => 'notice-info',
+						] );
+					}
 
 				// otherwise, a general getting started message
 				} elseif ( ! $this->is_plugin_settings() ) {
@@ -206,13 +224,8 @@ if ( ! class_exists( 'WC_Facebookcommerce' ) ) :
 						'</a>'
 					);
 
-					$message_id = 'get_started';
-				}
-
-				if ( $message ) {
-
-					$this->get_admin_notice_handler()->add_admin_notice( $message, self::PLUGIN_ID . '_' . $message_id, [
-						'dismissible'  => false,
+					$this->get_admin_notice_handler()->add_admin_notice( $message, self::PLUGIN_ID . '_get_started', [
+						'dismissible'  => true,
 						'notice_class' => 'notice-info',
 					] );
 				}
