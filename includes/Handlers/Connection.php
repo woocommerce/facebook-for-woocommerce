5041ea212c343188853f5eacaf28bf53a7c3409f
Willington Vega <wvega@wvega.com>
2020-07-13 17:50:14 -0500

    Add helper method to get the timezone string

--- includes/Handlers/Connection.php
+++ includes/Handlers/Connection.php
@@ -655,6 +655,28 @@ class Connection {
 
 
 	/**
+	 * Gets the configured timezone string using values accepted by Facebook
+	 *
+	 * @since 2.0.0-dev.1
+	 *
+	 * @return string
+	 */
+	private function get_timezone_string() {
+
+		$timezone = wc_timezone_string();
+
+		// convert +05:30 and +05:00 into Etc\GMT+5 - we ignore the minutes because Facebook does not allow minute offsets
+		if ( preg_match( '/([+-])(\d{2}):\d{2}/', $timezone, $matches ) ) {
+
+			$hours    = (int) $matches[2];
+			$timezone = "Etc\GMT{$matches[1]}{$hours}";
+		}
+
+		return $timezone;
+	}
+
+
+	/**
 	 * Stores the given ID value.
 	 *
 	 * @since 2.0.0-dev.1
