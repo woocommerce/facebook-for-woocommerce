b4727c0070d5c389af55a3f3a040fa526226e55d
Deiva Magalhaes <deiva@skyverge.com>
2020-08-21 13:31:38 -0700

    Add Product_Categories class

---
+++ includes/Product_Categories.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Helper class with utility methods for getting and setting various product category values.
+ *
+ * @since 2.3.0-dev.1
+ */
+class Product_Categories {
+
+
+}
