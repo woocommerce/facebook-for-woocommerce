d5b963096e2005026f1a77d57bc6afe745d6896e
Deiva Magalhaes <deiva@skyverge.com>
2020-08-21 12:26:11 -0700

    Add Commerce class

---
+++ includes/Commerce.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Base handler for Commerce-specific functionality.
+ *
+ * @since 2.3.0-dev.1
+ */
+class Commerce {
+
+
+}
