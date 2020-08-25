50122a29a455997ae740e97319057393e99e0fd1
Deiva Magalhaes <deiva@skyverge.com>
2020-08-25 10:09:56 -0700

    Remove recursive call

--- includes/Products.php
+++ includes/Products.php
@@ -672,26 +672,28 @@ class Products {
 		if ( $product->is_type( 'variation' ) ) {
 
 			// get the attribute from the parent
-			$parent_product = wc_get_product( $product->get_parent_id() );
-
-			return self::get_product_color_attribute( $parent_product );
+			$product = wc_get_product( $product->get_parent_id() );
 		}
 
-		$meta_value     = $product->get_meta( self::COLOR_ATTRIBUTE_META_KEY );
 		$attribute_name = '';
 
-		// check if an attribute with that name exists
-		if ( self::product_has_attribute( $product, $meta_value ) ) {
-			$attribute_name = $meta_value;
-		}
+		if ( $product ) {
 
-		if ( empty( $attribute_name ) ) {
-			// try to find a matching attribute
-			foreach ( self::get_available_product_attributes( $product ) as $attribute ) {
+			$meta_value = $product->get_meta( self::COLOR_ATTRIBUTE_META_KEY );
 
-				if ( stripos( $attribute->get_name(), 'color' ) !== false || stripos( $attribute->get_name(), 'colour' ) !== false ) {
-					$attribute_name = $attribute->get_name();
-					break;
+			// check if an attribute with that name exists
+			if ( self::product_has_attribute( $product, $meta_value ) ) {
+				$attribute_name = $meta_value;
+			}
+
+			if ( empty( $attribute_name ) ) {
+				// try to find a matching attribute
+				foreach ( self::get_available_product_attributes( $product ) as $attribute ) {
+
+					if ( stripos( $attribute->get_name(), 'color' ) !== false || stripos( $attribute->get_name(), 'colour' ) !== false ) {
+						$attribute_name = $attribute->get_name();
+						break;
+					}
 				}
 			}
 		}
