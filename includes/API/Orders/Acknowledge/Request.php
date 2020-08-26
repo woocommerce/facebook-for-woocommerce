8d18fd64135b0326ebb363c7801958ef19d704da
Deiva Magalhaes <deiva@skyverge.com>
2020-08-26 16:16:00 -0700

    Implement the constructor

--- includes/API/Orders/Acknowledge/Request.php
+++ includes/API/Orders/Acknowledge/Request.php
@@ -25,4 +25,23 @@ class Request extends API\Request  {
 	use API\Traits\Idempotent_Request;
 
 
+	/**
+	 * API request constructor.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @param string $remote_id remote order ID
+	 * @param string $merchant_order_reference WC order ID
+	 */
+	public function __construct( $remote_id, $merchant_order_reference ) {
+
+		parent::__construct( "/{$remote_id}", 'POST' );
+
+		$this->set_data( [
+			'merchant_order_reference' => $merchant_order_reference,
+			'idempotency_key'          => $this->get_idempotency_key(),
+		] );
+	}
+
+
 }
