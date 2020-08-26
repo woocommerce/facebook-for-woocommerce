cbad4017a7a9cfb9b8ab347f2d0a2ecab33b191e
Deiva Magalhaes <deiva@skyverge.com>
2020-08-26 11:37:11 -0700

    Add the Orders read request

---
+++ includes/API/Orders/Read/Request.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Orders\Read;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\Facebook\API;
+
+/**
+ * Orders API read request object.
+ *
+ * @since 2.1.0-dev.1
+ */
+class Request extends API\Request  {
+
+
+	/**
+	 * API request constructor.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @param string $remote_id remote order ID
+	 * @param array $fields fields to be returned
+	 */
+	public function __construct( $remote_id, $fields = [] ) {
+
+		parent::__construct( "/{$remote_id}", 'GET' );
+
+		if ( ! empty( $fields ) ) {
+
+			$params['fields'] = is_array( $fields ) ? implode( ',', $fields ) : $fields;
+
+		} else {
+
+			// request all top-level fields
+			$params['fields'] = implode( ',', [
+				'id',
+				'order_status',
+				'created',
+				'last_updated',
+				'items',
+				'ship_by_date',
+				'merchant_order_id',
+				'channel',
+				'selected_shipping_option',
+				'shipping_address',
+				'estimated_payment_details',
+				'buyer_details',
+			] );
+		}
+	}
+
+
+}
