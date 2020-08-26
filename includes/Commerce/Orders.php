2e539a84950cf58da15eddc592551776bf307dcf
Deiva Magalhaes <deiva@skyverge.com>
2020-08-25 18:44:29 -0700

    Add method stubs

--- includes/Commerce/Orders.php
+++ includes/Commerce/Orders.php
@@ -10,6 +10,8 @@
 
 namespace SkyVerge\WooCommerce\Facebook\Commerce;
 
+use SkyVerge\WooCommerce\PluginFramework\v5_5_4\SV_WC_Plugin_Exception;
+
 defined( 'ABSPATH' ) or exit;
 
 /**
@@ -25,6 +27,145 @@ class Orders {
 
 	/** @var string the meta key used to store the remote order ID */
 	const REMOTE_ID_META_KEY = '_wc_facebook_commerce_remote_id';
+
+
+	/**
+	 * Finds a local order based on the Commerce ID stored in REMOTE_ID_META_KEY.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @param string $remote_id Commerce order ID
+	 * @return \WC_Order|null
+	 */
+	public function find_local_order( $remote_id ) {
+
+		// TODO: implement
+		return null;
+	}
+
+
+	/**
+	 * Creates a local WooCommerce order based on an Orders API order object.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @param API\Orders\Order $remote_order Orders API order object
+	 * @return \WC_Order
+	 * @throws SV_WC_Plugin_Exception
+	 */
+	public function create_local_order( $remote_order ) {
+
+		// TODO: implement
+		return null;
+	}
+
+
+	/**
+	 * Updates a local WooCommerce order based on an Orders API order object.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @param API\Orders\Order $remote_order Orders API order object
+	 * @param \WC_Order $local_order local order object
+	 * @return \WC_Order
+	 */
+	public function update_local_order( $remote_order, $local_order ) {
+
+		// TODO: implement
+		return $local_order;
+	}
+
+
+	/**
+	 * Updates WooCommerce’s Orders by fetching orders from the API and either creating or updating local orders.
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function update_local_orders() {
+
+		// TODO: implement
+	}
+
+
+	/**
+	 * Frequency in seconds that orders are updated.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @return int
+	 */
+	public function get_order_update_interval() {
+
+		// TODO: implement
+		return 5 * MINUTE_IN_SECONDS;
+	}
+
+
+	/**
+	 * Schedules a recurring ACTION_FETCH_ORDERS action, if not already scheduled.
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function schedule_local_orders_update() {
+
+		// TODO: implement
+	}
+
+
+	/**
+	 * Adds the necessary action & filter hooks.
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function add_hooks() {
+
+		// TODO: implement
+	}
+
+
+	/**
+	 * Fulfills an order via API.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @param \WC_Order $order order object
+	 * @param string $tracking_number shipping tracking number
+	 * @param string $carrier shipping carrier
+	 * @throws SV_WC_Plugin_Exception
+	 */
+	public function fulfill_order( $order, $tracking_number, $carrier ) {
+
+		// TODO: implement
+	}
+
+
+	/**
+	 * Refunds an order.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @param \WC_Order $refund order refund object
+	 * @param mixed $args
+	 * @throws SV_WC_Plugin_Exception
+	 */
+	public function add_order_refund( $refund, $args ) {
+
+		// TODO: implement
+	}
+
+
+	/**
+	 * Cancels an order.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @param \WC_Order $order order object
+	 * @throws SV_WC_Plugin_Exception
+	 */
+	public function cancel_order( $order ) {
+
+		// TODO: implement
+	}
 
 
 }
