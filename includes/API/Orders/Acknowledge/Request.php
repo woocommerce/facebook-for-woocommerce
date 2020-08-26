5a47531e0243518597aa5da927ea493019d0ce40
Deiva Magalhaes <deiva@skyverge.com>
2020-08-26 16:12:21 -0700

    Add the Orders acknowledge request

---
+++ includes/API/Orders/Acknowledge/Request.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Orders\Acknowledge;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\Facebook\API;
+
+/**
+ * Orders API acknowledge request object.
+ *
+ * @since 2.1.0-dev.1
+ */
+class Request extends API\Request  {
+
+
+}
