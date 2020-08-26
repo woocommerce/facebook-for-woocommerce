a2137fd7e067040e7f617c66be559eced90c64cc
Deiva Magalhaes <deiva@skyverge.com>
2020-08-25 19:13:19 -0700

    Add constants

--- includes/API/Orders/Order.php
+++ includes/API/Orders/Order.php
@@ -19,4 +19,18 @@ defined( 'ABSPATH' ) or exit;
  */
 class Order {
 
+
+	/** @var string API state meaning Facebook is still processing the order and no action is possible */
+	const STATUS_PROCESSING = 'FB_PROCESSING';
+
+	/** @var string API state meaning Facebook has processed the orders and the seller needs to acknowledge it */
+	const STATUS_CREATED = 'CREATED';
+
+	/** @var string API state meaning the order was acknowledged and is now being processed in WC */
+	const STATUS_IN_PROGRESS = 'IN_PROGRESS';
+
+	/** @var string API state meaning all items in the order are shipped and/or cancelled */
+	const STATUS_COMPLETED = 'COMPLETED';
+
+
 }
