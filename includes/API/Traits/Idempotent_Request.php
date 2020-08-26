bf520849e4c4f51d3dcd7b8e371640c9a2a9a8dc
Deiva Magalhaes <deiva@skyverge.com>
2020-08-26 13:07:01 -0700

    Create Idempotent_Request trait

---
+++ includes/API/Traits/Idempotent_Request.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Traits;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Idempotent request trait.
+ *
+ * @since 2.1.0-dev.1
+ */
+trait Idempotent_Request {
+
+}
