e24f7c5ad3b37611ab2ad3d146abec38f65e5905
Deiva Magalhaes <deiva@skyverge.com>
2020-08-25 19:01:10 -0700

    Create API\Orders\Order class

---
+++ includes/API/Orders/Order.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Orders;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Orders API order handler.
+ *
+ * @since 2.1.0-dev.1
+ */
+class Order {
+
+}
