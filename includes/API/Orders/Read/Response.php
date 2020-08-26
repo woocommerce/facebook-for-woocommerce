84b6e07c5ed1634801a26632af6a5d23b143e07d
Deiva Magalhaes <deiva@skyverge.com>
2020-08-26 15:50:37 -0700

    Add the Orders read response

---
+++ includes/API/Orders/Read/Response.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Orders\Read;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\Facebook\API;
+
+/**
+ * Orders API read response object.
+ *
+ * @since 2.1.0-dev.1
+ */
+class Response extends API\Response {
+
+
+}
