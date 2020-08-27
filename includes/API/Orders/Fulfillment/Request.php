8a190c80f0280a12c0cb53820d61ee81bd743060
Deiva Magalhaes <deiva@skyverge.com>
2020-08-26 18:23:01 -0700

    Implement the Orders fulfillment request constructor

--- includes/API/Orders/Fulfillment/Request.php
+++ includes/API/Orders/Fulfillment/Request.php
@@ -25,4 +25,22 @@ class Request extends API\Request  {
 	use API\Traits\Idempotent_Request;
 
 
+	/**
+	 * API request constructor.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @param string $remote_id remote order ID
+	 * @param array $fulfillment_data fulfillment data
+	 */
+	public function __construct( $remote_id, $fulfillment_data ) {
+
+		parent::__construct( "/{$remote_id}/shipments", 'POST' );
+
+		$fulfillment_data['idempotency_key'] = $this->get_idempotency_key();
+
+		$this->set_data( $fulfillment_data );
+	}
+
+
 }
