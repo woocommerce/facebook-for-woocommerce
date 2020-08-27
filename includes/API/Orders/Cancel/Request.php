c51b131046f59361e55cb0a86c030ffee7393387
Deiva Magalhaes <deiva@skyverge.com>
2020-08-26 18:36:35 -0700

    Add the Orders cancel request

---
+++ includes/API/Orders/Cancel/Request.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\API\Orders\Cancel;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\Facebook\API;
+
+/**
+ * Orders API cancel request object.
+ *
+ * @since 2.1.0-dev.1
+ */
+class Request extends API\Request  {
+
+
+	use API\Traits\Idempotent_Request;
+
+
+}
