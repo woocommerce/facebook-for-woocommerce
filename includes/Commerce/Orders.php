2f8ad4c4b3b644b9dd866cb479c83d13bdf3cb0c
Deiva Magalhaes <deiva@skyverge.com>
2020-08-27 17:36:19 -0700

    Do not set shipping total twice

--- includes/Commerce/Orders.php
+++ includes/Commerce/Orders.php
@@ -142,7 +142,7 @@ class Orders {
 		$estimated_payment_details = $remote_order->get_estimated_payment_details();
 
 		// TODO: confirm if we should use $estimated_payment_details['subtotal']->items for something
-		$local_order->set_shipping_total( $estimated_payment_details['subtotal']->shipping);
+		// TODO: confirm if we should use $estimated_payment_details['subtotal']->shipping for something
 		// TODO: confirm if we should use $estimated_payment_details['tax'] for something
 		$local_order->set_total( $estimated_payment_details['total_amount']->amount );
 		// TODO: confirm if we should use $estimated_payment_details['tax_remitted'] for something
