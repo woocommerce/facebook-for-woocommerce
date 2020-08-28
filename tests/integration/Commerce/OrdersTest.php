f47aa1cf6f231b85f3747d1e248f17b30bdc8027
Chase Wiseman <chase@skyverge.com>
2020-08-28 14:18:26 -0700

    Add test for the scheduled action

--- tests/integration/Commerce/OrdersTest.php
+++ tests/integration/Commerce/OrdersTest.php
@@ -16,6 +16,13 @@ class OrdersTest extends \Codeception\TestCase\WPTestCase {
 	/** Test methods **************************************************************************************************/
 
 
+	/** @see Orders::schedule_local_orders_update() */
+	public function test_schedule_local_orders_update() {
+
+		$this->assertNotFalse( as_next_scheduled_action( Orders::ACTION_FETCH_ORDERS, [], \WC_Facebookcommerce::PLUGIN_ID ) );
+	}
+
+
 	/** @see Orders::find_local_order() */
 	public function test_find_local_order_found() {
 
