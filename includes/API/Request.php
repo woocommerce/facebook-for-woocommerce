f9077987a480b4437f5b9ac63c226b72ef528cd2
Chase Wiseman <chase@skyverge.com>
2020-09-03 16:58:32 -0700

    Add request retry methods

--- includes/API/Request.php
+++ includes/API/Request.php
@@ -25,6 +25,16 @@ class Request extends Framework\SV_WC_API_JSON_Request {
 	use Traits\Rate_Limited_Request;
 
 
+	/** @var int number of retries to attempt if told to do so by Facebook */
+	protected $retry_limit = 5;
+
+	/** @var int number of times this request has been retried */
+	protected $retry_count = 0;
+
+	/** @var int[] the response codes that should trigger */
+	protected $retry_codes = [];
+
+
 	/**
 	 * API request constructor.
 	 *
@@ -63,6 +73,56 @@ class Request extends Framework\SV_WC_API_JSON_Request {
 	public function set_data( $data ) {
 
 		$this->data = $data;
+	}
+
+
+	/**
+	 * Gets the number of times this request has been retried.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @return int
+	 */
+	public function get_retry_count() {
+
+		return $this->retry_count;
+	}
+
+
+	/**
+	 * Marks the request as having been retried.
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function mark_retry() {
+
+		$this->retry_count++;
+	}
+
+
+	/**
+	 * Gets the number of retries to attempt if told to do so by Facebook.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @return int
+	 */
+	public function get_retry_limit() {
+
+		return $this->retry_limit;
+	}
+
+
+	/**
+	 * Response codes that should trigger a retry for this request.
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @return int[]
+	 */
+	public function get_retry_codes() {
+
+		return $this->retry_codes;
 	}
 
 
