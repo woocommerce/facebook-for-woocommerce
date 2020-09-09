ce9efe2c332a735444074add674dd7e8d35dabf2
Deiva Magalhaes <deiva@skyverge.com>
2020-09-09 16:28:01 -0700

    Add method stubs

--- includes/Admin/Orders.php
+++ includes/Admin/Orders.php
@@ -20,4 +20,169 @@ defined( 'ABSPATH' ) or exit;
 class Orders {
 
 
+	/**
+	 * Handler constructor.
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function __construct() {
+
+		$this->add_hooks();
+	}
+
+
+	/**
+	 * Adds the necessary action & filter hooks.
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function add_hooks() {
+
+	}
+
+
+	/**
+	 * Enqueue the assets.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function enqueue_assets() {
+
+	}
+
+
+	/**
+	 * Adds admin notices.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function add_notices() {
+
+	}
+
+
+	/**
+	 * Remove order metaboxes if the order is a Commerce pending order.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function maybe_remove_order_metaboxes() {
+
+	}
+
+
+	/**
+	 * Renders the Complete, Refund, & Cancel modals templates markup.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function render_modal_templates() {
+
+	}
+
+
+	/**
+	 * Renders the refund reason field.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function render_refund_reason_field() {
+
+	}
+
+
+	/**
+	 * Sends a refund request to the Commerce API when a WC refund is created.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function handle_refund() {
+
+	}
+
+
+	/**
+	 * Sets a transient to display a notice regarding bulk updates for Commerce orders' statuses.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function handle_bulk_update() {
+
+	}
+
+
+	/**
+	 * Prevents sending emails for Commerce orders.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @return bool
+	 */
+	public function maybe_stop_order_email() {
+
+		return true;
+	}
+
+
+	/**
+	 * Returns whether or not the order is editable.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @return bool
+	 */
+	public function is_order_editable() {
+
+		return true;
+	}
+
+
+	/**
+	 * Returns whether or not the current screen is an orders screen.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @return bool
+	 */
+	public function is_orders_screen() {
+
+		return true;
+	}
+
+
+	/**
+	 * Returns whether or not the current screen is an order edit screen.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @return bool
+	 */
+	public function is_edit_order_screen() {
+
+		return true;
+	}
+
+
 }
