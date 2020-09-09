4fcaa5cf82a0b84fcd64bb0964c68824f29ef3de
Deiva Magalhaes <deiva@skyverge.com>
2020-09-09 16:08:09 -0700

    Create the Admin\Orders class

---
+++ includes/Admin/Orders.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\Admin;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * General handler for order admin functionality.
+ *
+ * @since 2.1.0-dev.1
+ */
+class Orders {
+
+
+}
