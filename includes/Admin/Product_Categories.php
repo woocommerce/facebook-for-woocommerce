eec3f65f81fa7586b8717971713d027dd5d6c03c
Deiva Magalhaes <deiva@skyverge.com>
2020-09-09 17:09:13 -0700

    Add method stubs

--- includes/Admin/Product_Categories.php
+++ includes/Admin/Product_Categories.php
@@ -24,4 +24,90 @@ class Product_Categories {
 	const FIELD_GOOGLE_PRODUCT_CATEGORY_ID = 'wc_facebook_google_product_category_id';
 
 
+	/**
+	 * Handler constructor.
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function __construct() {
+
+	}
+
+
+	/**
+	 * Enqueue the assets.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function enqueue_assets() {
+
+	}
+
+
+	/**
+	 * Renders the Google product category field markup for the add form.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function render_add_google_product_category_field() {
+
+	}
+
+
+	/**
+	 * Renders the Google product category field markup for the edit form.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function render_edit_google_product_category_field() {
+
+	}
+
+
+	/**
+	 * Renders the common tooltip markup.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 */
+	public function render_google_product_category_tooltip() {
+
+	}
+
+
+	/**
+	 * Gets the common field title.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @return string
+	 */
+	public function get_google_product_category_field_title() {
+
+	}
+
+
+	/**
+	 * Saves the POSTed Google product category ID and triggers a sync for the affected products.
+	 *
+	 * @internal
+	 *
+	 * @since 2.1.0-dev.1
+	 *
+	 * @return string
+	 */
+	public function save_google_product_category() {
+
+	}
+
+
 }
