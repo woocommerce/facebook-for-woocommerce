f2e915afc06116eafb89efe1095443987cdf4f7c
Deiva Magalhaes <deiva@skyverge.com>
2020-09-11 15:57:35 -0700

    Add stub methods

--- assets/js/admin/google-product-category-fields.js
+++ assets/js/admin/google-product-category-fields.js
@@ -21,6 +21,68 @@ jQuery( document ).ready( ( $ ) => {
 	class WC_Facebook_Google_Product_Category_Fields {
 
 
+		/**
+		 * Handler constructor.
+		 *
+		 * @since 2.1.0-dev.1
+		 *
+		 * @param {Object[]} categories The full categories list, indexed by the category ID
+		 * @param {string} categories[].label The category label
+		 * @param {string[]} categories[].options The category's child categories' IDs
+		 * @param {string} categories[].parent The category's parent category ID
+		 * @param {string} input_id The element that should receive the latest concrete category ID
+		 */
+		constructor(categories, input_id) {
+
+			this.categories = categories;
+
+			this.input_id = input_id;
+
+			// TODO: add wrapper div
+
+			// TODO: add first two selects
+		}
+
+
+		/**
+		 * Updates the subsequent selects whenever one of the selects changes.
+		 *
+		 * @since 2.1.0-dev.1
+		 */
+		onChange(element) {
+
+			// TODO: implement
+		}
+
+
+		/**
+		 * Adds a new select with the given options.
+		 *
+		 * @since 2.1.0-dev.1
+		 *
+		 * @param {string[]} options The select options, indexed by the option ID
+		 */
+		addSelect(options) {
+
+			// TODO: implement
+		}
+
+
+		/**
+		 * Gets an array of options for the given category ID.
+		 *
+		 * @since 2.1.0-dev.1
+		 *
+		 * @param {string} category_id The given category ID
+		 * @return {string[]} the select options, indexed by the option ID
+		 */
+		getOptions(category_id) {
+
+			// TODO: implement
+		}
+
+
 	}
+
 
 } );
