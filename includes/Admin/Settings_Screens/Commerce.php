ed1bf1ae7e2cff62b7190c14dd539d2582397179
Deiva Magalhaes <deiva@skyverge.com>
2020-09-11 10:24:37 -0700

    Add the Commerce settings screen class

---
+++ includes/Admin/Settings_Screens/Commerce.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\Admin\Settings_Screens;
+
+defined( 'ABSPATH' ) or exit;
+
+use SkyVerge\WooCommerce\Facebook\Admin;
+
+/**
+ * The Commerce settings screen object.
+ */
+class Commerce extends Admin\Abstract_Settings_Screen {
+
+}
