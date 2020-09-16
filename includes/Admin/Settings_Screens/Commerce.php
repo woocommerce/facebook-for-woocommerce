d91fd9f9df5689c4d7ebc4cf77e77d953aa69c54
Chase Wiseman <chase@skyverge.com>
2020-09-16 10:07:06 -0700

    Remove funky github addition

--- includes/Admin/Settings_Screens/Commerce.php
+++ includes/Admin/Settings_Screens/Commerce.php
@@ -105,7 +105,6 @@ class Commerce extends Admin\Abstract_Settings_Screen {
 
 		// build the proxy app URL where the user will land after onboarding, to be redirected to the site URL
 		$redirect_url = add_query_arg( 'site_url', urlencode( $site_url ), facebook_for_woocommerce()->get_connection_handler()->get_proxy_url() );
-```
 
 		// build the final connect URL, direct to Facebook
 		$connect_url = add_query_arg( [
