891da5707e02b0c16bffc95e271e331c33283150
Chase Wiseman <chase@skyverge.com>
2020-09-16 14:02:58 -0700

    Update the scopes tests

--- tests/integration/Handlers/ConnectionTest.php
+++ tests/integration/Handlers/ConnectionTest.php
@@ -133,6 +133,9 @@ class ConnectionTest extends \Codeception\TestCase\WPTestCase {
 			'manage_business_extension' => [ 'manage_business_extension' ],
 			'catalog_management'        => [ 'catalog_management' ],
 			'business_management'       => [ 'business_management' ],
+			'ads_management'            => [ 'ads_management' ],
+			'ads_read'                  => [ 'ads_read' ],
+			'pages_read_engagement'     => [ 'pages_read_engagement' ],
 		];
 	}
 
