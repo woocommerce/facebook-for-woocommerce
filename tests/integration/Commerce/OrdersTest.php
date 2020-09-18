fb0671ee0003e0496f8d68c982eeff8bf5fb6c99
Adriano Castro <adrianostanley@gmail.com>
2020-09-18 09:29:59 -0300

    Add a test assertion to the order note

--- tests/integration/Commerce/OrdersTest.php
+++ tests/integration/Commerce/OrdersTest.php
@@ -217,6 +217,8 @@ class OrdersTest extends \Codeception\TestCase\WPTestCase {
 		$this->assertEquals( $response_data['id'], $updated_local_order->get_meta( Orders::REMOTE_ID_META_KEY ) );
 
 		$this->assertEquals( 'processing', $updated_local_order->get_status() );
+
+		$this->assertTrue( $this->order_has_note( $updated_local_order, sprintf( 'Order %s paid in %s', $response_data['id'], $response_data['channel'] ) ) );
 	}
 
 
