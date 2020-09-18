c999adeeb0ce0f07454dd224dad6cf8eed42e1e2
Willington Vega <wvega@wvega.com>
2020-09-17 21:19:38 -0500

    Show a modal if the selected category changes

--- assets/js/admin/settings-commerce.js
+++ assets/js/admin/settings-commerce.js
@@ -6,3 +6,36 @@
  *
  * @package FacebookCommerce
  */
+
+ jQuery( document ).ready( function( $ ) {
+
+	let $form = $( 'form.wc-facebook-settings' );
+	let $defaultCategoryField = $( '#wc_facebook_google_product_category_id' );
+	let defaultCategoryId = $defaultCategoryField.val();
+
+	$form.on( 'submit', function( event ) {
+
+		if ( $form.data( 'allow-submit' ) || ! $defaultCategoryField.val() || $defaultCategoryField.val() === defaultCategoryId ) {
+			return;
+		}
+
+		event.preventDefault();
+
+		$( '#wc-backbone-modal-dialog .modal-close' ).trigger( 'click' );
+
+		new $.WCBackboneModal.View( {
+			target: 'facebook-for-woocommerce-modal',
+			string: {
+				message: facebook_for_woocommerce_settings_commerce.default_google_product_category_modal_message,
+				buttons: facebook_for_woocommerce_settings_commerce.default_google_product_category_modal_buttons
+			}
+		} );
+
+		$( document.body )
+			.off( 'wc_backbone_modal_response.facebook_for_commerce' )
+			.on( 'wc_backbone_modal_response.facebook_for_commerce', function() {
+				$form.data( 'allow-submit', true ).find( ':submit' ).trigger( 'click' );
+			} );
+	} );
+
+ } );
