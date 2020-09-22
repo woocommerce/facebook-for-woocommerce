52b7e545640fbeb613795cc9f96b489384709675
Deiva Magalhaes <deiva@skyverge.com>
2020-09-21 17:12:21 -0700

    Add test for save_commerce_fields()

--- tests/integration/Admin/ProductsTest.php
+++ tests/integration/Admin/ProductsTest.php
@@ -80,7 +80,30 @@ class ProductsTest extends \Codeception\TestCase\WPTestCase {
 
 	// TODO: add test for render_commerce_fields()
 
-	// TODO: add test for save_commerce_fields()
+
+	/** @see Products::save_commerce_fields() */
+	public function test_save_commerce_fields() {
+		global $post;
+
+		$product = $this->tester->get_product( [ 'attributes' => $this->tester->create_product_attributes() ] );
+		$post    = get_post( $product->get_id() );
+
+		$_POST[ Admin\Products::FIELD_COMMERCE_ENABLED ] = 'yes';
+		$_POST[ Admin\Products::FIELD_GOOGLE_PRODUCT_CATEGORY_ID ] = '1234';
+		$_POST[ Admin\Products::FIELD_GENDER ] = 'male';
+		$_POST[ Admin\Products::FIELD_COLOR ] = 'color';
+		$_POST[ Admin\Products::FIELD_SIZE ] = 'size';
+		$_POST[ Admin\Products::FIELD_PATTERN ] = 'pattern';
+
+		$this->get_products_handler()->save_commerce_fields( $product );
+
+		$this->assertEquals( true, \SkyVerge\WooCommerce\Facebook\Products::is_commerce_enabled_for_product( $product ) );
+		$this->assertEquals( '1234', \SkyVerge\WooCommerce\Facebook\Products::get_google_product_category_id( $product ) );
+		$this->assertEquals( 'male', \SkyVerge\WooCommerce\Facebook\Products::get_product_gender( $product ) );
+		$this->assertEquals( 'color', \SkyVerge\WooCommerce\Facebook\Products::get_product_color_attribute( $product ) );
+		$this->assertEquals( 'size', \SkyVerge\WooCommerce\Facebook\Products::get_product_size_attribute( $product ) );
+		$this->assertEquals( 'pattern', \SkyVerge\WooCommerce\Facebook\Products::get_product_pattern_attribute( $product ) );
+	}
 
 
 	/** Utility methods ***********************************************************************************************/
