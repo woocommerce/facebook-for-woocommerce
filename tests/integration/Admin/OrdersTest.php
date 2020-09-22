c74e1ad3dbdf0e5109c15f4d5bdddabfc24c6908
Adriano Castro <adrianostanley@gmail.com>
2020-09-22 13:30:59 -0300

    Add the test methods for is_order_editable

--- tests/integration/Admin/OrdersTest.php
+++ tests/integration/Admin/OrdersTest.php
@@ -126,7 +126,41 @@ class OrdersTest extends \Codeception\TestCase\WPTestCase {
 	}
 
 
-	// TODO: add test for is_order_editable()
+	/**
+	 * @see Admin\Orders::is_order_editable()
+	 *
+	 * @param bool $maybe_editable
+	 * @param string $created_via
+	 * @param string $status
+	 * @param bool $expected
+	 *
+	 * @dataProvider provider_is_order_editable
+	 *
+ 	 * @throws WC_Data_Exception
+	 */
+	public function test_is_order_editable( $maybe_editable, $created_via, $status, $expected ) {
+
+		$order = new \WC_Order();
+		$order->set_created_via( $created_via );
+		$order->set_status( $status );
+		$order->save();
+
+		$this->assertEquals( $expected, $this->get_orders_handler()->is_order_editable( $maybe_editable, $order ) );
+	}
+
+
+	/** @see test_is_order_editable */
+	public function provider_is_order_editable() {
+
+		return [
+			[ false, 'checkout',  'pending',    false ],
+			[ true,  'checkout',  'pending',    true ],
+			[ true,  'instagram', 'pending',    false ],
+			[ true,  'instagram', 'processing', true ],
+			[ true,  'facebook',  'pending',    false ],
+			[ true,  'facebook',  'processing', true ],
+		];
+	}
 
 
 	/** Utility methods ***********************************************************************************************/
