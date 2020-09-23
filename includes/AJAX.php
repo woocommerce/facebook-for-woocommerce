0156b3f3191acdc2ce927e449675155b08cfb9ab
Chase Wiseman <chase@skyverge.com>
2020-09-23 13:39:07 -0700

    Throw an exception if the tracking number is missing when completing an order via AJAX

--- includes/AJAX.php
+++ includes/AJAX.php
@@ -117,9 +117,14 @@ class AJAX {
 			}
 
 			$order_id        = (int) Framework\SV_WC_Helper::get_posted_value( 'order_id' );
+			$tracking_number = wc_clean( Framework\SV_WC_Helper::get_posted_value( 'tracking_number' ) );
 
 			if ( empty( $order_id ) ) {
 				throw new Framework\SV_WC_Plugin_Exception( __( 'Order ID is required', 'facebook-for-woocommerce' ), 400 );
+			}
+
+			if ( empty( $tracking_number ) ) {
+				throw new Framework\SV_WC_Plugin_Exception( __( 'Tracking number is required', 'facebook-for-woocommerce' ), 400 );
 			}
 
 		} catch ( Framework\SV_WC_Plugin_Exception $exception ) {
