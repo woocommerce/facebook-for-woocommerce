8f358d291613c5af058e0e3401caefee1764fae0
Nabeel Molham <n.molham@gmail.com>
2020-09-24 18:44:45 +0200

    Determine either to open complete order modal first or send ajax request directly

--- assets/js/admin/orders.js
+++ assets/js/admin/orders.js
@@ -11,4 +11,48 @@ jQuery( document ).ready( ( $ ) => {
 
 	'use strict';
 
+	const isCommerceOrder = Boolean( wc_facebook_commerce_orders.is_commerce_order );
+
+	let $form             = $( 'form[id="post"]' );
+	let $orderStatusField = $( '#order_status' );
+	let shipmentTracking  = wc_facebook_commerce_orders.shipment_tracking;
+	let trackingNumber    = Array.isArray( shipmentTracking ) && shipmentTracking[ 0 ] ? shipmentTracking[ 0 ] : shipmentTracking;
+
+
+	/**
+	 * Displays the order complete modal on order form submit
+	 */
+	function displayCompleteModal() {
+
+		console.log( 'Complete Modal' );
+	}
+
+
+	/**
+	 * Make complete order AJAX Request
+	 */
+	function makeCompleteAjaxRequest() {
+
+		console.log( 'Complete AJAX' );
+	}
+
+
+	$form.on( 'submit', function ( event ) {
+
+		if ( !isCommerceOrder || $form.data( 'allow-submit' ) ) {
+			return;
+		}
+
+		if ( 'wc-completed' === $orderStatusField.val() ) {
+
+			event.preventDefault();
+
+			if ( trackingNumber.length ) {
+				makeCompleteAjaxRequest();
+			} else {
+				displayCompleteModal();
+			}
+		}
+	} );
+
 } );
