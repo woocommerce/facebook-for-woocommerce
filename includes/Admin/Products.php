2942f1efb1c83a78e687c8990b04991fcfcb34ca
Willington Vega <wvega@wvega.com>
2020-09-28 09:19:19 -0500

    Render the checked attribute when Commerce is enabled only

--- includes/Admin/Products.php
+++ includes/Admin/Products.php
@@ -228,7 +228,7 @@ class Products {
 			<input type="checkbox" class="enable-if-sync-enabled"
 			       name="<?php echo esc_attr( self::FIELD_COMMERCE_ENABLED ); ?>"
 			       id="<?php echo esc_attr( self::FIELD_COMMERCE_ENABLED ); ?>" value="yes"
-			       checked="<?php echo Products_Handler::is_commerce_enabled_for_product( $product ) ? 'checked' : ''; ?>">
+			       <?php checked( Products_Handler::is_commerce_enabled_for_product( $product ) ); ?> />
 		</p>
 
 		<div id="product-not-ready-notice" style="display:none;">
