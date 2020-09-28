8371cdeb3138ef5e922248099efa48beab3c1394
Willington Vega <wvega@wvega.com>
2020-09-28 09:19:19 -0500

    Add hidden field fallback for Commerce Enabled checkbox

--- includes/Admin/Products.php
+++ includes/Admin/Products.php
@@ -224,6 +224,7 @@ class Products {
 				<span class="woocommerce-help-tip"
 				      data-tip="<?php echo esc_attr_e( 'Enable to sell this product on Instagram. Products that are hidden in the Facebook catalog can be synced, but won’t be available for purchase.', 'facebook-for-woocommerce' ); ?>"></span>
 			</label>
+			<input type="hidden" name="<?php echo esc_attr( self::FIELD_COMMERCE_ENABLED ); ?>" value="no" />
 			<input type="checkbox" class="enable-if-sync-enabled"
 			       name="<?php echo esc_attr( self::FIELD_COMMERCE_ENABLED ); ?>"
 			       id="<?php echo esc_attr( self::FIELD_COMMERCE_ENABLED ); ?>" value="yes"
