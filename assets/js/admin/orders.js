b269df7cab1beea8dbb4b7376b6b4d69c61df80f
Willington Vega <wvega@wvega.com>
2020-09-28 16:55:23 -0500

    Clone the Facebook reason field to reuse the original

--- assets/js/admin/orders.js
+++ assets/js/admin/orders.js
@@ -401,7 +401,7 @@ jQuery( document ).ready( ( $ ) => {
 	function moveRefundReasonField() {
 
 		let $oldRefundReasonField  = $( '#refund_reason' );
-		let $newRefundReasonField  = $( '#wc_facebook_refund_reason' ).css( 'width', $oldRefundReasonField.css( 'width' ) );
+		let $newRefundReasonField  = $( '#wc_facebook_refund_reason' ).clone().css( 'width', $oldRefundReasonField.css( 'width' ) );
 		let $refundReasonRow       = $oldRefundReasonField.closest( 'tr' );
 		let $refundDescriptionRow  = $refundReasonRow.clone();
 
