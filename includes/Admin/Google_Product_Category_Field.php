0230869cfad7475a043bc3a3321b86d2c06e15a4
Nabeel Molham <n.molham@gmail.com>
2020-09-29 14:19:51 +0200

    Use `\n` line break instead of environment dependant `PHP_EOL` constant

--- includes/Admin/Google_Product_Category_Field.php
+++ includes/Admin/Google_Product_Category_Field.php
@@ -92,7 +92,7 @@ class Google_Product_Category_Field {
 		if ( is_array( $categories_response ) && isset( $categories_response['body'] ) ) {
 
 			$categories_body = $categories_response['body'];
-			$categories_body = explode( PHP_EOL, $categories_body );
+			$categories_body = explode( "\n", $categories_body );
 
 			// format: ID - Top level category > ... > Parent category > Category label
 			// example: 7385 - Animals & Pet Supplies > Pet Supplies > Bird Supplies > Bird Cage Accessories
