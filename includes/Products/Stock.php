8357f2916ff3c19c1db15e24e4d2b6b790003096
Willington Vega <wvega@wvega.com>
2020-10-06 19:57:37 -0500

    Add Products Stock handler

---
+++ includes/Products/Stock.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook\Products;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * The product stock handler.
+ *
+ * @since 2.0.0
+ */
+class Stock {
+
+
+}
