0dcbb106526fab16b0e1bc2485ef1d810ce4ea11
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-10-28 16:27:00 +0800

    Remove Create ad from connection box and rework HTML output

--- includes/Admin/Settings_Screens/Connection.php
+++ includes/Admin/Settings_Screens/Connection.php
@@ -256,38 +256,11 @@ class Connection extends Admin\Abstract_Settings_Screen {
 		}
 
 		$subtitle = __( 'Use this WooCommerce and Facebook integration to:', 'facebook-for-woocommerce' );
-
 		$benefits = [
 			__( 'Create an ad in a few steps', 'facebook-for-woocommerce'),
 			__( 'Use built-in best practices for online sales', 'facebook-for-woocommerce'),
 			__( 'Get reporting on sales and revenue', 'facebook-for-woocommerce'),
 		];
-
-		if ( $is_connected ) {
-
-			$actions = [
-				'create-ad' => [
-					'label' => __( 'Create Ad', 'facebook-for-woocommerce' ),
-					'type'  => 'primary',
-					'url'   => 'https://www.facebook.com/ad_center/create/ad/?entry_point=facebook_ads_extension&page_id=' . facebook_for_woocommerce()->get_integration()->get_facebook_page_id(),
-				],
-				'manage' => [
-					'label' => __( 'Manage Connection', 'facebook-for-woocommerce' ),
-					'type'  => 'secondary',
-					'url'   => facebook_for_woocommerce()->get_connection_handler()->get_manage_url(),
-				],
-			];
-
-		} else {
-
-			$actions = [
-				'get-started' => [
-					'label' => __( 'Get Started', 'facebook-for-woocommerce' ),
-					'type'  => 'primary',
-					'url'   => facebook_for_woocommerce()->get_connection_handler()->get_connect_url(),
-				],
-			];
-		}
 
 		?>
 
@@ -306,22 +279,20 @@ class Connection extends Admin\Abstract_Settings_Screen {
 
 			<div class="actions">
 
-				<?php foreach ( $actions as $action_id => $action ) : ?>
-
-					<a
-						href="<?php echo esc_url( $action['url'] ); ?>"
-						class="button button-<?php echo esc_attr( $action['type'] ); ?>"
-						<?php echo ( 'get-started' !== $action_id ) ? 'target="_blank"' : ''; ?>
-					>
-						<?php echo esc_html( $action['label'] ); ?>
-					</a>
-
-				<?php endforeach; ?>
-
 				<?php if ( $is_connected ) : ?>
+
+					<a href="<?php echo esc_url( facebook_for_woocommerce()->get_connection_handler()->get_manage_url() ); ?>" class="button button-secondary">
+						<?php esc_html_e( 'Manage Connection', 'facebook-for-woocommerce' ); ?>
+					</a>
 
 					<a href="<?php echo esc_url( facebook_for_woocommerce()->get_connection_handler()->get_disconnect_url() ); ?>" class="uninstall">
 						<?php esc_html_e( 'Uninstall', 'facebook-for-woocommerce' ); ?>
+					</a>
+
+				<?php else : ?>
+
+					<a href="<?php echo esc_url( facebook_for_woocommerce()->get_connection_handler()->get_connect_url() ); ?>" class="button button-primary" target="_blank">
+						<?php esc_html_e( 'Get Started', 'facebook-for-woocommerce' ); ?>
 					</a>
 
 				<?php endif; ?>
