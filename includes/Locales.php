963896e7dc1c56d5a183b12b140b97033c48a60b
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-10-28 18:20:27 +0800

    Add locales handler

---
+++ includes/Locales.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @package FacebookCommerce
+ */
+
+namespace SkyVerge\WooCommerce\Facebook;
+
+defined( 'ABSPATH' ) or exit;
+
+/**
+ * Helper class with utility methods for handling locales in Facebook.
+ *
+ * @since 2.2.0-dev.1
+ */
+class Locales {
+
+
+
+
+}
