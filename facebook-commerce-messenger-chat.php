fddabda3506edcdc9021d820ab4c86e46d33326e
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-10-28 18:24:44 +0800

    Move private locales property to static method

--- facebook-commerce-messenger-chat.php
+++ facebook-commerce-messenger-chat.php
@@ -8,6 +8,8 @@
  * @package FacebookCommerce
  */
 
+use SkyVerge\WooCommerce\Facebook\Locales;
+
 if ( ! class_exists( 'WC_Facebookcommerce_MessengerChat' ) ) :
 
 	if ( ! class_exists( 'WC_Facebookcommerce_Utils' ) ) {
@@ -16,112 +18,6 @@ if ( ! class_exists( 'WC_Facebookcommerce_MessengerChat' ) ) :
 
 	class WC_Facebookcommerce_MessengerChat {
 
-
-		/** @var string[] list of supported locales */
-		private static $supported_locales = [
-			'af_ZA',
-			'ar_AR',
-			'as_IN',
-			'az_AZ',
-			'be_BY',
-			'bg_BG',
-			'bn_IN',
-			'br_FR',
-			'bs_BA',
-			'ca_ES',
-			'cb_IQ',
-			'co_FR',
-			'cs_CZ',
-			'cx_PH',
-			'cy_GB',
-			'da_DK',
-			'de_DE',
-			'el_GR',
-			'en_GB',
-			'en_US',
-			'es_ES',
-			'es_LA',
-			'et_EE',
-			'eu_ES',
-			'fa_IR',
-			'ff_NG',
-			'fi_FI',
-			'fo_FO',
-			'fr_CA',
-			'fr_FR',
-			'fy_NL',
-			'ga_IE',
-			'gl_ES',
-			'gn_PY',
-			'gu_IN',
-			'ha_NG',
-			'he_IL',
-			'hi_IN',
-			'hr_HR',
-			'hu_HU',
-			'hy_AM',
-			'id_ID',
-			'is_IS',
-			'it_IT',
-			'ja_JP',
-			'ja_KS',
-			'jv_ID',
-			'ka_GE',
-			'kk_KZ',
-			'km_KH',
-			'kn_IN',
-			'ko_KR',
-			'ku_TR',
-			'lt_LT',
-			'lv_LV',
-			'mg_MG',
-			'mk_MK',
-			'ml_IN',
-			'mn_MN',
-			'mr_IN',
-			'ms_MY',
-			'mt_MT',
-			'my_MM',
-			'nb_NO',
-			'ne_NP',
-			'nl_BE',
-			'nl_NL',
-			'nn_NO',
-			'or_IN',
-			'pa_IN',
-			'pl_PL',
-			'ps_AF',
-			'pt_BR',
-			'pt_PT',
-			'qz_MM',
-			'ro_RO',
-			'ru_RU',
-			'rw_RW',
-			'sc_IT',
-			'si_LK',
-			'sk_SK',
-			'sl_SI',
-			'so_SO',
-			'sq_AL',
-			'sr_RS',
-			'sv_SE',
-			'sw_KE',
-			'sz_PL',
-			'ta_IN',
-			'te_IN',
-			'tg_TJ',
-			'th_TH',
-			'tl_PH',
-			'tr_TR',
-			'tz_MA',
-			'uk_UA',
-			'ur_PK',
-			'uz_UZ',
-			'vi_VN',
-			'zh_CN',
-			'zh_HK',
-			'zh_TW',
-		];
 
 
 		public function __construct( $settings ) {
@@ -201,7 +97,7 @@ if ( ! class_exists( 'WC_Facebookcommerce_MessengerChat' ) ) :
 
 			if ( class_exists( 'Locale' ) ) {
 
-				foreach ( self::$supported_locales as $locale ) {
+				foreach ( Locales::get_supported_locales() as $locale ) {
 
 					if ( $name = \Locale::getDisplayName( $locale, substr( $locale, 0, 2 ) ) ) {
 						$locales[ $locale ] = ucfirst( $name );
@@ -214,7 +110,7 @@ if ( ! class_exists( 'WC_Facebookcommerce_MessengerChat' ) ) :
 
 				$translations = wp_get_available_translations();
 
-				foreach ( self::$supported_locales as $locale ) {
+				foreach ( Locales::get_supported_locales() as $locale ) {
 
 					if ( isset( $translations[ $locale ]['native_name'] ) ) {
 
