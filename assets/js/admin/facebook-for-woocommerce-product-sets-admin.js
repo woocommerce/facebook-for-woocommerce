4ee1ca5b72164acd85f6433d1e2e8372c4abeead
Enrique Colasurdo <enrique@saucal.com>
2020-11-17 20:11:07 -0300

    Change Product Set Admin JS to clear select2 after inserting new Term

--- assets/js/admin/facebook-for-woocommerce-product-sets-admin.js
+++ assets/js/admin/facebook-for-woocommerce-product-sets-admin.js
@@ -9,7 +9,23 @@
 
 jQuery( document ).ready( function( $ ) {
 
-	jQuery( '.select2.wc-facebook' ).select2().addClass( 'visible' ).attr( 'disabled', false );
-	jQuery( '.select2.updating-message' ).addClass( 'hidden' );
+	if ( jQuery( '.select2.wc-facebook' ).length ) {
+		wcFacebookProductSetInit();
+	}
+
+	function wcFacebookProductSetInit() {
+
+		jQuery( '.select2.wc-facebook' ).select2().addClass( 'visible' ).attr( 'disabled', false );
+		jQuery( '.select2.updating-message' ).addClass( 'hidden' );
+
+		jQuery( document ).ajaxSuccess( function( e, request, settings ) {
+			var obj = new URLSearchParams( settings.data )
+			if ( obj.has( 'action' ) && 'add-tag' === obj.get( 'action' ) && obj.has( 'taxonomy' ) && 'fb_product_set' === obj.get( 'taxonomy' ) ) {
+	console.log('YEAH');
+				jQuery( '.select2.wc-facebook' ).select2().val( null ).trigger( 'change' );
+			}
+	    });
+
+	}
 
 } );
