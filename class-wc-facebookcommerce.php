870000a79d23bcea834ee6c03f087178466be8d2
Willington Vega <wvega@wvega.com>
2020-11-23 17:48:13 -0500

    Bump dev version to 2.3.0-beta.3

--- class-wc-facebookcommerce.php
+++ class-wc-facebookcommerce.php
@@ -22,7 +22,7 @@ if ( ! class_exists( 'WC_Facebookcommerce' ) ) :
 
 
 		/** @var string the plugin version */
-		const VERSION = '2.3.0-beta.2';
+		const VERSION = '2.3.0-beta.3';
 
 		/** @var string for backwards compatibility TODO: remove this in v2.0.0 {CW 2020-02-06} */
 		const PLUGIN_VERSION = self::VERSION;
