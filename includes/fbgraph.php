fb338b87306cc956c3b8dbe03e266524d05f5c30
Enrique Colasurdo <enrique@saucal.com>
2021-02-17 16:32:54 -0300

    Add optional parameter to Product Set delete method By default will send the allow_live_product_set_deletion parameter as true

--- includes/fbgraph.php
+++ includes/fbgraph.php
@@ -407,7 +407,11 @@ if ( ! class_exists( 'WC_Facebookcommerce_Graph_API' ) ) :
 		}
 
 		public function delete_product_set_item( $product_set_id ) {
-			$url = $this->build_url( $product_set_id, '', '8.0' );
+
+			$params = ( true === apply_filters( 'wc_facebook_commerce_allow_live_product_set_deletion', $product_set_id ) ) ? '?allow_live_product_set_deletion=true' : '';
+
+			$url = $this->build_url( $product_set_id, $params, '9.0' );
+
 			return self::_delete( $url );
 		}
 
