868197398fa9256626ec81289702c5d27c59ed7c
Dennis Kwon <dkwon@fb.com>
2021-02-26 12:30:38 -0500

    Unschedule fetch order actions if disconnected

--- includes/Commerce/Orders.php
+++ includes/Commerce/Orders.php
@@ -521,7 +521,13 @@ class Orders {
 	 * @since 2.1.0
 	 */
 	public function schedule_local_orders_update() {
-		if ( facebook_for_woocommerce()->get_commerce_handler()->is_connected() && false === as_next_scheduled_action( self::ACTION_FETCH_ORDERS, [], \WC_Facebookcommerce::PLUGIN_ID ) ) {
+		// only schedule if connected
+		if ( ! facebook_for_woocommerce()->get_commerce_handler()->is_connected() ) {
+			as_unschedule_all_actions( self::ACTION_FETCH_ORDERS );
+			return;
+		}
+
+		if ( ! as_next_scheduled_action( self::ACTION_FETCH_ORDERS, [], \WC_Facebookcommerce::PLUGIN_ID ) ) {
 
 			$interval = $this->get_order_update_interval();
 
