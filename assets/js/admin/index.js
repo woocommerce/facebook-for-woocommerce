e34c4a6e3d48fc4ac16ab2b8175c127b8a57cdb1
Dan Bitzer <danielbitzer@gmail.com>
2021-03-24 14:57:15 +1030

    Use webpack to minify existing JS files

---
+++ assets/js/admin/index.js
@@ -0,0 +1,1 @@
+// Add new any JS here
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,30 @@
 // Load the default @wordpress/scripts config object
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
+const legacyAdminFileNames = [
+	'google-product-category-fields',
+	'infobanner',
+	'metabox',
+	'modal',
+	'orders',
+	'product-categories',
+	'product-sets-admin',
+	'products-admin',
+	'settings-commerce',
+	'settings-sync',
+];
+
+const legacyAdminFileEntries = {};
+
+legacyAdminFileNames.forEach( ( name ) => {
+	legacyAdminFileEntries[ `admin/${ name }` ] = `./assets/js/admin/${ name }.js`;
+} );
+
 module.exports = {
 	...defaultConfig,
 	entry: {
-		frontend: './assets/js/frontend/index.js',
-		admin: './assets/js/admin/index.js',
+		'admin/index.js': './assets/js/admin/index.js',
+		...legacyAdminFileEntries,
 	},
 	output: {
 		filename: '[name].js',
