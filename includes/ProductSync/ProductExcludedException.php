1394ec9aba8716f65fc234c17cac1c18d605f9e1
Dan Bitzer <danielbitzer@gmail.com>
2021-05-07 13:33:58 +0930

    Add ProductExcludedException exception class

---
+++ includes/ProductSync/ProductExcludedException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SkyVerge\WooCommerce\Facebook\ProductSync;
+
+use Exception;
+
+/**
+ * Class ProductExcludedException
+ *
+ * Exception for when a product is excluded from Facebook product sync.
+ */
+class ProductExcludedException extends Exception {}
