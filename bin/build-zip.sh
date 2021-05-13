c6e101860840966e52c8164b8ac3bf2fcabbc52b
Dan Bitzer <danielbitzer@gmail.com>
2021-05-13 14:32:37 +0930

    Minor improvements to build-zip.sh

--- bin/build-zip.sh
+++ bin/build-zip.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
+# This script can only be run from the project directory
+
 PLUGIN_SLUG="facebook-for-woocommerce"
 PROJECT_PATH=$(pwd)
 BUILD_PATH="${PROJECT_PATH}/build"
+SAKE_ZIP="${BUILD_PATH}/facebook-for-woocommerce.*.zip"
 FINAL_ZIP="${PROJECT_PATH}/${PLUGIN_SLUG}.zip"
-
-echo $PROJECT_PATH
 
 echo "Removing last zip..."
 rm $FINAL_ZIP
+rm $SAKE_ZIP
 
-echo "Installing PHP and JS dependencies..."
+echo "Installing JS dependencies..."
 npm ci || exit
 
 echo "Building @wordpress/scripts assets..."
-npm run build:assets
+npm run build:assets || exit
 
 # Sake installs composer and makes a ZIP
 echo "Building zip with Sake..."
 npx sake zip || exit
 
 echo "Copying zip to project folder..."
-find $BUILD_PATH -name "facebook-for-woocommerce.*.zip" -exec cp {} $FINAL_ZIP \; || exit;
+cp $SAKE_ZIP $FINAL_ZIP || exit
 
 echo "Build done!"
