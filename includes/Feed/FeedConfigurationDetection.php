dca4ef10faff0ef21ee3d0d870c3fc92b3dc7a3c
haszari <rua@automattic.com>
2021-06-01 14:30:07 +1200

    improve and standardise error handling: - separate methods for getting data & saving to transient - all error paths throw - top-level catch logs error to prevent fatal errors

--- includes/Feed/FeedConfigurationDetection.php
+++ includes/Feed/FeedConfigurationDetection.php
@@ -24,16 +24,29 @@ class FeedConfigurationDetection {
 	}
 
 	/**
-	 * Get config settings for feed-based sync for WooCommerce Tracker.
+	 * Store config settings for feed-based sync for WooCommerce Tracker.
 	 *
 	 * Gets various settings related to the feed, and data about recent uploads.
 	 * This is formatted into an array of keys/values, and saved to a transient for inclusion in tracker snapshot.
+	 * Note this does not send the data to tracker - this happens later (see Tracker class).
 	 *
-	 * @throws Error
 	 * @since x.x.x
 	 * @return Array Key-value array of various configuration settings.
 	 */
-	public function get_data_source_feed_tracker_info() {
+	public function track_data_source_feed_tracker_info() {
+		try {
+			$info = $this->get_data_source_feed_tracker_info;
+			facebook_for_woocommerce()->get_tracker()->track_facebook_feed_config( $info );
+		} catch ( \Error $error ) {
+			facebook_for_woocommerce()->log( 'Unable to detect valid feed configuration: ' . $error->getMessage() );
+		}
+	}
+
+	/**
+	 * Get config settings for feed-based sync for WooCommerce Tracker.
+	 * @return Array Key-value array of various configuration settings.
+	 */
+	private function get_data_source_feed_tracker_info() {
 		$integration         = facebook_for_woocommerce()->get_integration();
 		$graph_api           = $integration->get_graph_api();
 		$integration_feed_id = $integration->get_feed_id();
@@ -43,38 +56,28 @@ class FeedConfigurationDetection {
 
 		// No catalog id. Most probably means that we don't have a valid connection.
 		if ( '' === $catalog_id ) {
-			facebook_for_woocommerce()->log( 'No catalog ID!' );
-			return false;
+			throw new Error( 'No catalog ID' );
 		}
 
 		// Get all feeds configured for the catalog.
-		try {
-			$feed_nodes = $this->get_feed_nodes_for_catalog( $catalog_id, $graph_api );
-		} catch ( \Error $th ) {
-			throw $th;
-		}
+		$feed_nodes = $this->get_feed_nodes_for_catalog( $catalog_id, $graph_api );
 
 		$info['feed_count'] = count( $feed_nodes );
 
 		// Check if the catalog has any feed configured.
 		if ( empty( $feed_nodes ) ) {
-			facebook_for_woocommerce()->log( 'No feed nodes!' );
-			return false;
+			throw new Error( 'No feed nodes for catalog' );
 		}
 
 		// Determine which is the most active feed (recently updated).
 		// Or "the" feed, if there is only one!
 		$active_feed_metadata = null;
 		foreach ( $feed_nodes as $feed ) {
-			try {
-				$metadata                       = $this->get_feed_metadata( $feed['id'], $graph_api );
-				$metadata['latest_upload_time'] = strtotime( $metadata['latest_upload'] );
-				if ( ! $active_feed_metadata ||
-					( $metadata['latest_upload_time'] > $active_feed_metadata['latest_upload_time'] ) ) {
-					$active_feed_metadata = $metadata;
-				}
-			} catch ( \Error $th) {
-				throw $th;
+			$metadata                       = $this->get_feed_metadata( $feed['id'], $graph_api );
+			$metadata['latest_upload_time'] = strtotime( $metadata['latest_upload'] );
+			if ( ! $active_feed_metadata ||
+				( $metadata['latest_upload_time'] > $active_feed_metadata['latest_upload_time'] ) ) {
+				$active_feed_metadata = $metadata;
 			}
 		}
 
@@ -95,18 +98,13 @@ class FeedConfigurationDetection {
 		$upload['end_time'] = $latest_upload['end_time'];
 
 		// Get more detailed metadata about the most recent feed upload.
-		try {
-			$upload_metadata = $this->get_feed_upload_metadata( $active_feed_metadata['latest_upload']['id'], $graph_api );
-		} catch ( \Error $th ) {
-			throw $th;
-		}
+		$upload_metadata = $this->get_feed_upload_metadata( $active_feed_metadata['latest_upload']['id'], $graph_api );
+
 		$upload['error_count']         = $upload_metadata['error_count'];
 		$upload['warning_count']       = $upload_metadata['warning_count'];
 		$upload['num_persisted_items'] = $upload_metadata['num_persisted_items'];
 
 		$info['active_feed']['latest_upload'] = $upload;
-
-		facebook_for_woocommerce()->get_tracker()->track_facebook_feed_config( $info );
 
 		return $info;
 	}
