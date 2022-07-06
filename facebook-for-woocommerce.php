971cf315fa6d3839d45f2f327b3ef09f61a59f6b
mikkamp <michael.kamp@automattic.com>
2022-07-06 16:01:55 +0100

    Product version bump update

--- facebook-for-woocommerce.php
+++ facebook-for-woocommerce.php
@@ -11,11 +11,11 @@
  * Description: Grow your business on Facebook! Use this official plugin to help sell more of your products using Facebook. After completing the setup, you'll be ready to create ads that promote your products and you can also create a shop section on your Page where customers can browse your products on Facebook.
  * Author: Facebook
  * Author URI: https://www.facebook.com/
- * Version: 2.6.16
+ * Version: 2.6.17
  * Text Domain: facebook-for-woocommerce
  * Tested up to: 6.0
  * WC requires at least: 3.5.0
- * WC tested up to: 6.6
+ * WC tested up to: 6.7
  * Requires PHP: 7.0
  *
  * @package FacebookCommerce
@@ -33,7 +33,7 @@ class WC_Facebook_Loader {
 	/**
 	 * @var string the plugin version. This must be in the main plugin file to be automatically bumped by Woorelease.
 	 */
-	const PLUGIN_VERSION = '2.6.16'; // WRCS: DEFINED_VERSION.
+	const PLUGIN_VERSION = '2.6.17'; // WRCS: DEFINED_VERSION.
 
 	// Minimum PHP version required by this plugin.
 	const MINIMUM_PHP_VERSION = '7.0.0';
