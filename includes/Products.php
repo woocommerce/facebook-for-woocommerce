dead9d1b5cd8e8f77f648f6bfa67cad62fa7604e
Rodrigue Tusse <rodrigue.tusse@automattic.com>
2022-08-26 12:50:39 +0200

    Update doc bloc comment.

--- includes/Products.php
+++ includes/Products.php
@@ -235,7 +235,8 @@ class Products {
 	/**
 	 * Determines whether the given product should be removed from the catalog.
 	 *
-	 * A product should be removed if it is no longer in stock and the user has opted-in to hide products that are out of stock.
+	 * A product should be removed if it is no longer in stock and the user has opted-in to hide products that are out of stock,
+	 * or belongs to an excluded category.
 	 *
 	 * @since 2.0.0
 	 *
@@ -244,7 +245,7 @@ class Products {
 	 */
 	public static function product_should_be_deleted( \WC_Product $product ) {
 
-		return 'yes' === get_option( 'woocommerce_hide_out_of_stock_items' ) && ! $product->is_in_stock() || ! facebook_for_woocommerce()->get_product_sync_validator( $product )->passes_product_terms_check();;
+		return ( 'yes' === get_option( 'woocommerce_hide_out_of_stock_items' ) && ! $product->is_in_stock() ) || ! facebook_for_woocommerce()->get_product_sync_validator( $product )->passes_product_terms_check();;
 	}
 
 
