8f19878bc5bf981d9516b76c6c66f19d5fb41f78
Dima <dmytro.maksiuta@automattic.com>
2022-10-17 18:16:26 +0300

    Moving exception file into a proper directory.

--- includes/Api/Exceptions/ConnectApiException.php
+++ includes/Api/Exceptions/ConnectApiException.php
@@ -7,7 +7,7 @@ defined( 'ABSPATH' ) or exit;
 
 /**
  * Class Connect_WC_API_Exception.
- * Exception is trown when Connection with FB fails. @see \WooCommerce\Facebook\Handlers\Connection
+ * Exception is thrown when Connection with FB fails. @see \WooCommerce\Facebook\Handlers\Connection
  *
  * @package WooCommerce\Facebook\Api\Exceptions
  */
