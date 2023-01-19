b84836f90169086d2236c8f66c99a96f1e5c31a5
Rodrigue Tusse <rodrigue.tusse@automattic.com>
2023-01-19 21:59:36 +0200

    Update the sync status when catalog visibility is edited.

--- assets/js/admin/products-admin.js
+++ assets/js/admin/products-admin.js
@@ -567,6 +567,17 @@ jQuery( document ).ready( function( $ ) {
 			toggleSyncAndShowOption( ! $( this ).prop( 'checked' ), simpleProductSyncModeSelect );
 		} ).trigger( 'change' );
 
+		// Update the sync when catalog visibility changes.
+		$( 'input[name=_visibility]' ).on ( 'change', function(){
+			if ( $( this ).val() !== 'hidden' && $( this ).val() !== 'search' ) {
+
+				if ( simpleProductSyncModeSelect.val() === 'sync_disabled' ) {
+					simpleProductSyncModeSelect.val( 'sync_and_show' ).trigger( 'change' );;
+				}
+				toggleSyncAndShowOption( true, simpleProductSyncModeSelect );
+			}
+		})
+
 		const $productData = $( '#woocommerce-product-data' );
 
 		// check whether the product meets the requirements for Commerce
