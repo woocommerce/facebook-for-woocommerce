21ae4192fde8e327510baf2021ccc2c1cd39f07a
Kruti Dugade <krutidugade@Krutis-MacBook-Pro.local>
2023-06-30 23:31:15 +0100

    Deletes the product from facebook once it is switched to draft.

--- facebook-commerce.php
+++ facebook-commerce.php
@@ -369,7 +369,7 @@ class WC_Facebookcommerce_Integration extends WC_Integration {
 		add_action( 'untrashed_post', [ $this, 'fb_restore_untrashed_variable_product' ] );
 
 		// Ensure product is deleted from FB when status is changed to draft.
-		add_action( 'publish_to_draft', [ $this, 'on_product_delete' ] );
+		add_action( 'publish_to_draft', [ $this, 'delete_draft_product' ] );
 
 		// Product Set hooks.
 		add_action( 'fb_wc_product_set_sync', [ $this, 'create_or_update_product_set_item' ], 99, 2 );
@@ -1019,6 +1019,22 @@ class WC_Facebookcommerce_Integration extends WC_Integration {
 	private function should_update_visibility_for_product_status_change( $new_status, $old_status ) {
 		return ( $old_status === 'publish' && $new_status !== 'publish' ) || ( $old_status === 'trash' && $new_status === 'publish' ) || ( $old_status === 'future' && $new_status === 'publish' );
 	}
+
+	/**
+	 * Deletes a product from Facebook when status is changed to draft.
+	 *
+	 * @param \WP_post $post
+	 */
+	public function delete_draft_product( $post ) {
+
+		if ( ! $post ) {
+			return;
+		}
+
+		$this->on_product_delete ( $post->ID );
+
+	}
+
 
 	/**
 	 * Generic function for use with any product publishing.
