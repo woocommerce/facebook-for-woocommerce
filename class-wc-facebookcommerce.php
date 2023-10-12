638f2134f4addcc209bfec2b204ddee5faf4c5f7
Kader Ibrahim S <kader.ibrahim.s@a8c.com>
2023-10-12 11:37:47 +0530

    Update class-wc-facebookcommerce.php
    Co-authored-by: Rodrigue <rodrigue.tusse@automattic.com>

--- class-wc-facebookcommerce.php
+++ class-wc-facebookcommerce.php
@@ -9,7 +9,7 @@
  * @package FacebookCommerce
  */
 
- require_once __DIR__ . '/includes/fbutils.php';
+require_once __DIR__ . '/includes/fbutils.php';
 
 use Automattic\WooCommerce\Admin\Features\Features as WooAdminFeatures;
 use Automattic\WooCommerce\Admin\Features\OnboardingTasks\TaskLists;
