8506846accace070a398c13527bbeacef915b14e
Bartosz Budzanowski <bartosz.budzanowski@gmail.com>
2023-10-17 22:30:10 +0300

    Revert "Fix test_on_product_save_existing_simple_product_sync_enabled_updates_the_product"
    This reverts commit 298c4f11720966f8a6085f202e9d5163b304d70b.

--- tests/Unit/WCFacebookCommerceIntegrationTest.php
+++ tests/Unit/WCFacebookCommerceIntegrationTest.php
@@ -529,8 +529,9 @@ class WCFacebookCommerceIntegrationTest extends WP_UnitTestCase {
 		$facebook_product_data['category']    = 1718;
 
 		$this->api->expects( $this->once() )
-			->method( 'update_product_item_batch_api' )
-			->with( 'facebook-product-item-id', $facebook_product_data );
+			->method( 'update_product_item' )
+			->with( 'facebook-product-item-id', $facebook_product_data )
+			->willReturn( new API\ProductCatalog\Products\Update\Response( '{"success":true}' ) );
 
 		$this->integration->on_product_save( $product_to_update->get_id() );
 
